5f5d100c156a4761e4812c3ce741e893dcdd169f
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-06-28 09:22:14 -0400

    Update to ZIO 2

--- morphir/knowledge/test/src/org/finos/morphir/knowledge/logic/MicrokanrenSpec.scala
+++ morphir/knowledge/test/src/org/finos/morphir/knowledge/logic/MicrokanrenSpec.scala
@@ -7,10 +7,10 @@ import zio.test._
 import zio.test.Assertion._
 import zio.test.TestAspect.{ignore, tag}
 
-object MicrokanrenSpec extends DefaultRunnableSpec {
+object MicrokanrenSpec extends ZIOSpecDefault {
   def spec = suite("Fact Spec")(
     suite("equal")(
-      testM("When 2 values are equal we should get back the initial state") {
+      test("When 2 values are equal we should get back the initial state") {
         val value1 = BigDecimal(19.99)
         val value2 = BigDecimal(19.99)
         val state  = State.empty
@@ -19,7 +19,7 @@ object MicrokanrenSpec extends DefaultRunnableSpec {
           actual <- sut(state).runCollect
         } yield assert(actual)((equalTo(Chunk(state))))
       },
-      testM("Calling equal, eq, and === should be equivalent") {
+      test("Calling equal, eq, and === should be equivalent") {
         import mk._
         val value1      = "Test-Value"
         val value2      = "Test-Value"
@@ -37,7 +37,7 @@ object MicrokanrenSpec extends DefaultRunnableSpec {
       }
     ),
     suite("or/disjunction")(
-      testM("When the first goal succeeds we should get back the state") {
+      test("When the first goal succeeds we should get back the state") {
         import mk._
         val state       = State.empty
         val successGoal = Goal.succeed
@@ -46,7 +46,7 @@ object MicrokanrenSpec extends DefaultRunnableSpec {
           results <- or(successGoal, failingGoal)(state).runCollect
         } yield assert(results)(equalTo(Chunk(state)))
       },
-      testM("When the first goal fails and second goal succeeds we should get back the state") {
+      test("When the first goal fails and second goal succeeds we should get back the state") {
         import mk._
         val state       = State.empty
         val successGoal = Goal.succeed
@@ -57,7 +57,7 @@ object MicrokanrenSpec extends DefaultRunnableSpec {
       }
     ),
     suite("and/conjunction")(
-      testM("Both goals must succeed for us to get back the state") {
+      test("Both goals must succeed for us to get back the state") {
         import mk._
         val state = State.empty
         val g1    = Goal.succeed
@@ -66,7 +66,7 @@ object MicrokanrenSpec extends DefaultRunnableSpec {
           results <- and(g1, g2)(state).runCollect
         } yield assert(results)(equalTo(Chunk(state)))
       },
-      testM("If the first goal fails the whole goal fails") {
+      test("If the first goal fails the whole goal fails") {
         import mk._
         val state = State.empty
         val g1    = Goal.fail
@@ -75,7 +75,7 @@ object MicrokanrenSpec extends DefaultRunnableSpec {
           results <- and(g1, g2)(state).runCollect
         } yield assert(results)(equalTo(Chunk.empty))
       },
-      testM("If the second goal fails the whole goal fails") {
+      test("If the second goal fails the whole goal fails") {
         import mk._
         val state = State.empty
         val g1    = Goal.succeed
--- morphir/knowledge/test/src/org/finos/morphir/knowledge/logic/core/FluxSpec.scala
+++ morphir/knowledge/test/src/org/finos/morphir/knowledge/logic/core/FluxSpec.scala
@@ -4,15 +4,15 @@ import zio.Chunk
 import zio.test._
 import zio.test.Assertion._
 
-object FluxSpec extends DefaultRunnableSpec {
+object FluxSpec extends ZIOSpecDefault {
   override def spec = suite("FluxSpec")(
-    testM("An empty flux should produce no values") {
+    test("An empty flux should produce no values") {
       val sut = Flux.empty[Int]
       for {
         res <- sut.runCollect
       } yield assert(res)(equalTo(Chunk.empty))
     },
-    testM("Results are interleaved") {
+    test("Results are interleaved") {
       val fives = Flux.repeat(5)
       val sixes = Flux.repeat(6)
       val sut   = fives <> sixes
@@ -20,7 +20,7 @@ object FluxSpec extends DefaultRunnableSpec {
         res <- sut.runCollectN(10)
       } yield assert(res)(equalTo(Chunk(5, 6, 5, 6, 5, 6, 5, 6, 5, 6)))
     },
-    testM("Results are interleaved when using mergeAll") {
+    test("Results are interleaved when using mergeAll") {
       val a_s    = Flux.repeat('a')
       val b_s    = Flux.repeat('b')
       val c_s    = Flux.repeat('c')
