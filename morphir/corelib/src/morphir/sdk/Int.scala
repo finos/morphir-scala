9f344d3852f87b75e6494f76f0a7e44d3d38d544
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-07-29 14:18:19 -0400

    Add additional Morphir SDK support in corelib (#32)

--- morphir/corelib/src/morphir/sdk/Int.scala
+++ morphir/corelib/src/morphir/sdk/Int.scala
@@ -1,7 +1,10 @@
 package morphir.sdk
-
+import morphir.interop.extern
 import scala.{ Int => SInt}
 
+@extern
 object Int:
   opaque type Int = SInt
 
+  private[sdk] def wrap(int:SInt):Int = int
+  private[sdk] def unwrap(int:Int):SInt = int
