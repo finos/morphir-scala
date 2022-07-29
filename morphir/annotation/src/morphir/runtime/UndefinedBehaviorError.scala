6a2f8a2b4e58466d00dffb0378f4ffbb738b838d
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-07-29 11:38:55 -0400

    Add some interop support items

---
+++ morphir/annotation/src/morphir/runtime/UndefinedBehaviorError.scala
@@ -0,0 +1,6 @@
+package morphir.runtime
+
+/**
+ * An exception that is thrown whenever an
+ */
+final class UndefinedBehaviorError extends java.lang.Error
---
+++ morphir/annotation/src/morphir/runtime/package.scala
@@ -0,0 +1,8 @@
+package morphir
+
+package object runtime {
+  @noinline def throwUndefined(): Nothing =
+    throw new UndefinedBehaviorError
+
+  def intrinsic:Nothing = throwUndefined()
+}
