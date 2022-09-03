fa42599f37ec08ec33efa6e30a6f55275641c7fd
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-09-02 21:44:33 -0400

    Playing with data trying to grab opaquetype with no type parameter

--- morphir/mscplugin/src/morphir/mscplugin/MirCodeGen.scala
+++ morphir/mscplugin/src/morphir/mscplugin/MirCodeGen.scala
@@ -13,14 +13,17 @@ import core.Denotations.NonSymSingleDenotation
 import core.Names.*
 import dotty.tools.FatalError
 import dotty.tools.dotc.printing.Printer
+import morphir.mir
 import morphir.mir.FQName
 import morphir.mir.Module.Definition as MorphirModuleDef
+import morphir.mir.Type.Definition as MorphirTypeDef
 import morphir.mir.module.ModuleName
 import morphir.mir.file.format.MirFile
 
 import io.bullet.borer.{Cbor, Output}
 import morphir.mir.MirFileSupport.given
 import java.io.OutputStream
+import zio.Chunk
 
 class MirCodeGen(val settings: GenMorphirIR.Settings)(using ctx:Context):
   import tpd._
@@ -50,11 +53,11 @@ class MirCodeGen(val settings: GenMorphirIR.Settings)(using ctx:Context):
     val allTypeDefs = collectTypeDefs(cunit.tpdTree)
     for (typeDef <- allTypeDefs)
       if (typeDef.symbol.is(ModuleClass))
-        val (moduleFQN, moduleDef) = genModuleData(typeDef)
-        val mirFile = MirFile(moduleFQN.getModuleName)
         println("====================================================")
         println(i"Module: ${typeDef.symbol}")
         println("----------------------------------------------------")
+        val (moduleFQN, moduleDef) = genModuleData(typeDef)
+        val mirFile = MirFile(moduleFQN.getModuleName)        
         println(i"ModuleName: ${moduleFQN.getModuleName}")
         println("====================================================")
         println()      
@@ -64,7 +67,6 @@ class MirCodeGen(val settings: GenMorphirIR.Settings)(using ctx:Context):
   end genCompilationUnit
 
   private def genModuleData(td:TypeDef):(FQName, MorphirModuleDef[Any,Any]) =
-    println(i"MIRCodeGen.genModuleData: ${td.name}")
     def collectTypeDefs(tree:Tree):List[TypeDef] =
       tree match
         case cd:TypeDef => 
@@ -83,40 +85,69 @@ class MirCodeGen(val settings: GenMorphirIR.Settings)(using ctx:Context):
     //TODO: Capture Source location information
     //implicit val pos:Position = sym.span        
 
-    td.tpe.typeMembers.foreach { d =>
-      println(s"symbol: ${d.symbol}, isOpaqueAlias: ${d.symbol.isOpaqueAlias}")
-      d match
-        case cd:ClassDenotation => 
-          println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")
-          println(i"|ClassDenotation: $cd")
-          println(i"|owner: ${cd.owner}")
-          println(i"prefix: ${cd.prefix}")
-          //
-          println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")      
-        case sd:SymDenotation =>
-          println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")
-          println(i"|SymDenotation: $sd")
-          println(i"prefix: ${sd.prefix}")
-          //
-          println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")      
-        case sd:NonSymSingleDenotation =>
-          println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")
-          println(i"|NonSymSingleDenotation: $sd")
+    // td.tpe.typeMembers.foreach { d =>
+    //   println(s"symbol: ${d.symbol}, isOpaqueAlias: ${d.symbol.isOpaqueAlias}")
+    //   d match
+    //     case cd:ClassDenotation => 
+    //       println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    //       println(i"|ClassDenotation: $cd")
+    //       println(i"|owner: ${cd.owner}")
+    //       println(i"prefix: ${cd.prefix}")
+    //       //
+    //       println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")      
+    //     case sd:SymDenotation =>
+    //       println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    //       println(i"|SymDenotation: $sd")
+    //       println(i"prefix: ${sd.prefix}")
+    //       //
+    //       println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")      
+    //     case sd:NonSymSingleDenotation if sd.symbol.isOpaqueAlias =>
+    //       println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    //       println("Processing Opaque type @@@@@@@@@")
+    //       println(i"|NonSymSingleDenotation: $sd")
           
-          println(i"| isTerm: ${sd.isTerm}, isType: ${sd.isType}")
-          println(i"|prefix: ${sd.prefix}")
-          println(i"|isTypeAlias: ${sd.info.isTypeAlias}, ${sd.showDcl}")
-          //
-          println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")      
-        case sd =>
-          println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")
-          println(i"|SingleDenotation: $sd")
+    //       println(i"| isTerm: ${sd.isTerm}, isType: ${sd.isType}")
+    //       println(i"|prefix: ${sd.prefix}")
+    //       println(i"|isTypeAlias: ${sd.info.isTypeAlias}, ${sd.showDcl}")
+    //       //
+    //       println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")      
+    //     case sd:NonSymSingleDenotation =>
+    //       println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    //       println(i"|NonSymSingleDenotation: $sd")
           
-          println(i"| isTerm: ${sd.isTerm}, isType: ${sd.isType}")
-          println(i"|prefix: ${sd.prefix}")
-          println(i"|isTypeAlias: ${sd.info.isTypeAlias}, ${sd.showDcl}")
-          //
-          println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")      
+    //       println(i"| isTerm: ${sd.isTerm}, isType: ${sd.isType}")
+    //       println(i"|prefix: ${sd.prefix}")
+    //       println(i"|isTypeAlias: ${sd.info.isTypeAlias}, ${sd.showDcl}")
+    //       //
+    //       println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")      
+    //     case sd =>
+    //       println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    //       println(i"|SingleDenotation: $sd")
+          
+    //       println(i"| isTerm: ${sd.isTerm}, isType: ${sd.isType}")
+    //       println(i"|prefix: ${sd.prefix}")
+    //       println(i"|isTypeAlias: ${sd.info.isTypeAlias}, ${sd.showDcl}")
+    //       //
+    //       println("|++++++++++++++++++++++++++++++++++++++++++++++++++++")      
+    // }
+
+    val types = td.tpe.typeMembers.collect {
+      case sd:NonSymSingleDenotation if sd.symbol.isOpaqueAlias =>                
+        println(i"TypeName: ${sd.name}")
+        val name = mir.Name.fromString(sd.name.show)
+        println(i"Name: ${name}")
+        println(i"TypeParams: ${sd.info.typeParams}")
+        val typeRef = mir.Type.reference("Placeholder.For.LHS")
+        val typeParams = Chunk.empty[mir.Name]
+        val typeAlias = mir.Type.Definition.TypeAlias(typeParams, typeRef)
+        val documented = mir.Documented("", typeAlias)
+        val accessControlled = mir.AccessControlled.publicAccess(documented)
+        (name, accessControlled)
+    }.toList
+
+    // Debug output
+    types.foreach{ case (name, typeAlias) =>
+      println(i"NamedType: ${(name, typeAlias)}")
     }
 
     val fqn = MorphirEncoding.encodeModuleName(sym)
