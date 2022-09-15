a17d331cd96eff0faf30109b555bbb7e1b827afd
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-09-14 23:12:46 -0400

    Changes to naming

--- morphir/ir/src/org/finos/morphir/ir/naming.scala
+++ morphir/ir/src/org/finos/morphir/ir/naming.scala
@@ -10,15 +10,19 @@ object naming:
     def apply(first: Token, rest: Token*): Name = first :: rest.toList
     def apply(tokens: ::[Token])                = tokens
 
-  sealed trait TypeName
-  enum Marker:
-    case FQN(packagePath: Path.PackagePath, modulePath: Path.ModulePath, name: LocalName)
-    case PackageName(path: Path.PackagePath)
-    case ModuleName(base: Path, name: LocalName)
-    case MemberName(value: String)
-    case RecordName(value: String)
-    case TypeVariableName(value: String)
-    case LocalName(value: String)
+  // TODO: Work through symbol so that you get basic structural info from the type
+  sealed trait TypeSym:
+    def name: Name
+
+  enum Symbol:
+    case GlobalSym(packagePath: Path.PackagePath, modulePath: Path.ModulePath, name: Option[LocalSym])
+    case PackageSym(path: Path.PackagePath)
+    case ModuleSym(base: Path, name: LocalSym)
+    case MemberSym(value: String)
+    case RecordSym(name: Name)     extends Symbol with TypeSym
+    case CustomTypeSym(name: Name) extends Symbol with TypeSym
+    case TypeVariableSym(name: Name)
+    case LocalSym(name: Name)
 
   sealed trait PathLike:
     def toList: List[Name]
@@ -27,15 +31,15 @@ object naming:
     case ModulePath(toList: List[Name])
     case PackagePath(toList: List[Name])
 
-  final case class QName(modulePath: Path.ModulePath, name: Marker.LocalName):
+  final case class QName(modulePath: Path.ModulePath, name: Symbol.LocalSym):
     self =>
-    def localName: Marker.LocalName       = self.name
-    def toTuple: (Path, Marker.LocalName) = (self.modulePath, self.name)
+    def localName: Symbol.LocalSym       = self.name
+    def toTuple: (Path, Symbol.LocalSym) = (self.modulePath, self.name)
 
   object QName:
-    def fromTuple(tuple: (Path.ModulePath, Marker.LocalName)): QName = QName(tuple._1, tuple._2)
+    def fromTuple(tuple: (Path.ModulePath, Symbol.LocalSym)): QName = QName(tuple._1, tuple._2)
 
-  final case class FQName(packagePath: Path.PackagePath, modulePath: Path.ModulePath, localName: Marker.LocalName)
+  final case class FQName(packagePath: Path.PackagePath, modulePath: Path.ModulePath, localName: Symbol.LocalSym)
 
   enum Global:
     case Module(packagePath: Path.PackagePath, modulePath: Path.ModulePath)
