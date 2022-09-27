7990fba1b9f65c3f9e6d30f7426d8e838daaf0dd
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-09-27 15:42:32 -0400

    Start modeling Morphir Elm CLI (but hit some ZIO CLI errors)

--- morphir/tools/cli/src/org/finos/morphir/Main.scala
+++ morphir/tools/cli/src/org/finos/morphir/Main.scala
@@ -2,26 +2,25 @@ package org.finos
 package morphir
 
 import java.nio.file.Paths
-import morphir.cli._
+import morphir.cli.{CliSetup, CliCommand, MorphirCliBuildInfo}
 import zio.Console.printLine
 import zio.cli.HelpDoc.Span.text
 import zio.cli._
 import zio.Scope
 import zio.ZIOAppArgs
 
-object Main extends CliDefault:
+object Main extends ZIOCliDefault:
   override def cliApp = CliApp.make(
     name = MorphirCliBuildInfo.product,
     version = MorphirCliBuildInfo.version,
     summary = text(MorphirCliBuildInfo.description),
     command = CliSetup.morphir
   ) {
-    case CommandData.Elm.Develop(port)       => printLine(s"Running elm develop on port $port")
-    case CommandData.Elm(args)               => printLine("Elm")
-    case cmd @ CommandData.Elm.Make(_, _, _) => printLine(s"Elm Make: $cmd")
-    case CommandData.Elm.Gen()               => printLine("Elm Gen")
-    case CommandData.Init()                  => printLine("Initializing...")
-    case CommandData.Setup                   => printLine("Setting up...")
-    case CommandData.Workspace()             => printLine("Workspace selected")
+    case CliCommand.Elm.Develop(port)       => printLine(s"Running elm develop on port $port")
+    case CliCommand.Elm(args)               => printLine("Elm")
+    case cmd @ CliCommand.Elm.Make(_, _, _) => printLine(s"Elm Make: $cmd")
+    case CliCommand.Elm.Gen()               => printLine("Elm Gen")
+    case CliCommand.Init()                  => printLine("Initializing...")
+    case CliCommand.Setup                   => printLine("Setting up...")
+    case CliCommand.Workspace()             => printLine("Workspace selected")
   }
-end Main
