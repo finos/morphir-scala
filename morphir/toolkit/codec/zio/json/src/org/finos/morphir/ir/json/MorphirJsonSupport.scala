05570556cddca46aab0b05772b6a291c789e388b
Dmitry Kozinets <dkozinets@yahoo.com>
2022-10-10 14:38:54 -0400

    Morphir IR Json Seialization

---
+++ morphir/toolkit/codec/zio/json/src/org/finos/morphir/ir/json/MorphirJsonSupport.scala
@@ -0,0 +1,4 @@
+package org.finos.morphir.ir.json
+
+trait MorphirJsonSupport  extends MorphirJsonEncodingSupport with MorphirJsonDecodingSupport
+object MorphirJsonSupport extends MorphirJsonSupport
