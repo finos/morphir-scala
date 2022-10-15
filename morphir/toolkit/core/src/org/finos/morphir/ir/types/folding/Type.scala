dabf33ebb580b05c4b1c0b2bcec059e03b7daf4a
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-10-14 22:51:35 -0400

    Define Tuple in folding implementation of Type

--- morphir/toolkit/core/src/org/finos/morphir/ir/types/folding/Type.scala
+++ morphir/toolkit/core/src/org/finos/morphir/ir/types/folding/Type.scala
@@ -8,7 +8,9 @@ import zio.Chunk
 
 sealed trait Type[+A] extends Product with Serializable { self =>
   import Type.{Unit => UnitType, _}
+
   final def ??(doc: String): Documented[Type[A]] = Documented(doc, self)
+
   def attributes: A
 
   final def foldContext[C, A1 >: A, Z](context: C)(folder: Folder[C, A1, Z]): Z = {
@@ -16,15 +18,18 @@ sealed trait Type[+A] extends Product with Serializable { self =>
     sealed trait TypeCase {
       def attributes: A1
     }
+    case class ExtensibleRecordCase(attributes: A1, name: Name, fieldSize: Int)     extends TypeCase
     case class FunctionCase(attributes: A1)                                         extends TypeCase
     case class RecordCase(attributes: A1, fieldSize: Int)                           extends TypeCase
     case class ReferenceCase(attributes: A1, typeName: FQName, typeParamsSize: Int) extends TypeCase
+    case class TupleCase(attributes: A1, arity: Int)                                extends TypeCase
 
     @tailrec
     def loop(in: List[Type[A]], out: List[Either[TypeCase, Z]]): List[Z] =
       in match {
         case ExtensibleRecord(attributes, name, fields) :: types =>
-          ???
+          val fieldTypeExprs = fields.map(_.data).toList
+          loop(fieldTypeExprs ++ types, Left(ExtensibleRecordCase(attributes, name, fields.size)) :: out)
         case Function(attributes, argumentType, returnType) :: types =>
           loop(argumentType :: returnType :: types, Left(FunctionCase(attributes)) :: out)
         case Record(attributes, fields) :: types =>
@@ -32,29 +37,40 @@ sealed trait Type[+A] extends Product with Serializable { self =>
           loop(fieldTypeExprs ++ types, Left(RecordCase(attributes, fields.size)) :: out)
         case Reference(attributes, typeName, typeParams) :: types =>
           loop(typeParams.toList ++ types, Left(ReferenceCase(attributes, typeName, typeParams.size)) :: out)
-        case Tuple(attributes, elements) :: types => ???
-        case UnitType(attributes) :: types        => loop(types, Right(unitCase(context, attributes)) :: out)
+        case Tuple(attributes, elements) :: types =>
+          loop(elements.toList ++ types, Left(TupleCase(attributes, elements.size)) :: out)
+        case UnitType(attributes) :: types => loop(types, Right(unitCase(context, attributes)) :: out)
         case Variable(attributes, name) :: types =>
           loop(types, Right(variableCase(context, attributes, name)) :: out)
         case Nil =>
           out.foldLeft[List[Z]](List.empty) {
             case (acc, Right(typ)) =>
               typ :: acc
+            case (acc, Left(ExtensibleRecordCase(attributes, name, fieldSize))) => ???
             case (acc, Left(FunctionCase(attributes))) =>
               val argumentType :: returnType :: rest = (acc: @unchecked)
               functionCase(context, attributes, argumentType, returnType) :: rest
+            case (acc, Left(RecordCase(attributes, fieldSize)))         => ???
             case (acc, Left(ReferenceCase(attributes, typeName, size))) =>
               // TODO: Is there a better way to do this?
               val typeParams = Chunk.fromIterable(acc.take(size))
               val rest       = acc.drop(size)
               referenceCase(context, attributes, typeName, typeParams) :: rest
-            case _ => ???
+            case (acc, Left(TupleCase(attributes, arity))) =>
+              val elements = Chunk.fromIterable(acc.take(arity))
+              val rest     = acc.drop(arity)
+              tupleCase(context, attributes, elements) :: rest
           }
       }
     loop(List(self), List.empty).head
   }
 
+  final def satisfies(check: PartialFunction[Type[A], Boolean]): Boolean =
+    check.lift(self).getOrElse(false)
+
   final def size: Int = foldContext(())(Type.Folder.Size)
+
+  final override def toString: String = foldContext(())(Type.Folder.ToString)
 }
 object Type extends TypeConstructors with UnattributedTypeConstructors {
   type FieldT[+A] = Field[Type[A]]
@@ -70,6 +86,11 @@ object Type extends TypeConstructors with UnattributedTypeConstructors {
   final case class Tuple[+A](attributes: A, elements: Chunk[Type[A]])                             extends Type[A]
   final case class Unit[+A](attributes: A)                                                        extends Type[A]
   final case class Variable[+A](attributes: A, name: Name)                                        extends Type[A]
+  object Variable {
+    def apply(name: String): Variable[Any]             = Variable((), Name.fromString(name))
+    def apply(name: Name): Variable[Any]               = Variable((), name)
+    def apply[A](attributes: A, name: String): Type[A] = Variable(attributes, Name.fromString(name))
+  }
 
   trait Folder[-Context, -Attrib, Z] {
     def extensibleRecordCase(context: Context, attributes: Attrib, name: Name, fields: Chunk[Field[Z]]): Z
@@ -89,14 +110,27 @@ object Type extends TypeConstructors with UnattributedTypeConstructors {
         argumentType + returnType + 1
       def recordCase(context: Any, attributes: Any, fields: Chunk[Field[Int]]): Int =
         fields.map(_.data).sum + 1
-      def referenceCase(context: Any, attributes: Any, typeName: FQName, typeParams: Chunk[Int]): Int = {
-        println(s"referenceCase: typeParams.size: ${typeParams.sum}")
+      def referenceCase(context: Any, attributes: Any, typeName: FQName, typeParams: Chunk[Int]): Int =
         typeParams.sum + 1
-      }
       def tupleCase(context: Any, attributes: Any, elements: Chunk[Int]): Int =
         elements.sum + 1
       def unitCase(context: Any, attributes: Any): Int                 = 1
       def variableCase(context: Any, attributes: Any, name: Name): Int = 1
+    }
+
+    object ToString extends Folder[Any, Any, String] {
+      def extensibleRecordCase(context: Any, attributes: Any, name: Name, fields: Chunk[Field[String]]): String =
+        s"ExtensibleRecord(${name}, ${fields.map(_.data).mkString(", ")})"
+      def functionCase(context: Any, attributes: Any, argumentType: String, returnType: String): String =
+        s"Function(${argumentType}, ${returnType})"
+      def recordCase(context: Any, attributes: Any, fields: Chunk[Field[String]]): String =
+        s"Record(${fields.map(_.data).mkString(", ")})"
+      def referenceCase(context: Any, attributes: Any, typeName: FQName, typeParams: Chunk[String]): String =
+        s"Reference(${typeName}, ${typeParams.mkString(", ")})"
+      def tupleCase(context: Any, attributes: Any, elements: Chunk[String]): String =
+        elements.mkString("(", ", ", ")")
+      def unitCase(context: Any, attributes: Any): String                 = "()"
+      def variableCase(context: Any, attributes: Any, name: Name): String = name.toCamelCase
     }
   }
 }
--- morphir/toolkit/core/test/src/org/finos/morphir/ir/types/folding/FoldingTypeSpec.scala
+++ morphir/toolkit/core/test/src/org/finos/morphir/ir/types/folding/FoldingTypeSpec.scala
@@ -1,20 +1,17 @@
-package org.finos.morphir.ir.types.folding
+package org.finos.morphir.ir.types
+package folding
+
 import zio.Chunk
 import org.finos.morphir.ir.{FQName, Name}
 import org.finos.morphir.testing.MorphirBaseSpec
 import zio.test._
+import Type._
 object FoldingTypeSpec extends MorphirBaseSpec {
   def spec = suite("FoldingTypeSpec")(
-    // foldContextSuite,
-    sizeSuite
-  )
-
-  def foldContextSuite = suite("foldContext")(
-    test("folding over Unit") {
-      val sut    = Type.unit
-      val actual = sut.foldContext(0)(Type.Folder.Size)
-      assertTrue(actual == 1)
-    }
+    sizeSuite,
+    tupleSuite,
+    unitSuite,
+    variableSuite
   )
 
   def sizeSuite = suite("size")(
@@ -24,19 +21,94 @@ object FoldingTypeSpec extends MorphirBaseSpec {
       assertTrue(actual == 1)
     },
     test("size of Variable") {
-      val sut    = Type.variable("x")
+      val sut    = variable("x")
       val actual = sut.size
       assertTrue(actual == 1)
     },
     test("size of simple Reference") {
-      val sut    = Type.Reference[Any]((), FQName.fromString("x"), Chunk.empty)
+      val sut    = Reference[Any]((), FQName.fromString("x"), Chunk.empty)
       val actual = sut.size
       assertTrue(actual == 1)
     },
     test("size of Reference with a single typeParam") {
-      val sut    = Type.Reference[Any]((), FQName.fromString("x"), Chunk(Type.Variable[Any]((), Name.fromString("y"))))
+      val sut    = Reference[Any]((), FQName.fromString("x"), Chunk(Type.Variable[Any]((), Name.fromString("y"))))
       val actual = sut.size
       assertTrue(actual == 2)
+    }
+  )
+
+  def tupleSuite = suite("Tuple")(
+    test("testing emptyTuple constructor") {
+      val sut = Type.emptyTuple("Attributes")
+      assertTrue(
+        sut.size == 1,
+        sut.toString == "()",
+        sut.attributes == "Attributes",
+        sut == Tuple("Attributes", Chunk.empty)
+      )
+    },
+    test("testing tuple constructor when given a Chunk") {
+      val helloVar = variable("hello")
+      val worldVar = variable("world")
+      val chunk    = Chunk(helloVar, worldVar)
+      val actual   = tuple(chunk)
+      assertTrue(
+        actual.size == 3,
+        actual.toString == "(hello, world)",
+        actual.attributes == (),
+        actual == Tuple((), chunk)
+      )
+    },
+    test("testing tuple constructor when given multiple un-attributed elements") {
+      val var1   = variable("one")
+      val var2   = variable("two")
+      val var3   = variable("three")
+      val actual = tuple(var1, var2, var3)
+      assertTrue(
+        actual.size == 4,
+        actual.toString == "(one, two, three)",
+        actual.attributes == (),
+        actual == Tuple((), Chunk(var1, var2, var3))
+      )
+    },
+    test("testing tuple with attributes constructor") {
+      val varA   = variable("A", "a")
+      val varB   = variable("B", "b")
+      val varC   = variable("C", "c")
+      val actual = tuple("(a, b, c)", varA, varB, varC)
+      assertTrue(
+        actual.size == 4,
+        actual.toString == "(a, b, c)",
+        actual.attributes == "(a, b, c)",
+        actual == Tuple("(a, b, c)", Chunk(varA, varB, varC))
+      )
+    }
+  )
+
+  def unitSuite = suite("Unit")(
+    test("Unit has size 1") {
+      val actual = Type.unit.size
+      assertTrue(actual == 1)
+    },
+    test("Unit toString should produce the expected result") {
+      val actual = Type.unit.toString
+      assertTrue(actual == "()")
+    }
+  )
+
+  def variableSuite = suite("Variable")(
+    test("Variable has size 1") {
+      val actual = variable("x").size
+      assertTrue(actual == 1)
+    },
+    test("testing first variable constructor") {
+      val actual = variable("FizzBuzz")
+      assertTrue(
+        // actual.satisfies { case Variable(_, name) => name.toString == "[fizz, buzz]" },
+        actual == Variable[Any]((), "FizzBuzz"),
+        actual.toString == "fizzBuzz",
+        actual.size == 1
+      )
     }
   )
 }
