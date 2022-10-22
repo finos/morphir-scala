81a890ec0160a8607c8a11ddc8e9161d8442bccb
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-10-22 14:15:44 -0400

    FieldSpec and ModuleModuleSpec are passing again

--- morphir/toolkit/core/test/src/org/finos/morphir/ir/FieldSpec.scala
+++ morphir/toolkit/core/test/src/org/finos/morphir/ir/FieldSpec.scala
@@ -1,25 +1,25 @@
-// package org.finos.morphir
-// package ir
+package org.finos.morphir
+package ir
 
-// import org.finos.morphir.ir.StringOps
-// import org.finos.morphir.testing.MorphirBaseSpec
-// import zio.test._
+import org.finos.morphir.ir.StringOps
+import org.finos.morphir.testing.MorphirBaseSpec
+import zio.test._
 
-// object FieldSpec extends MorphirBaseSpec {
-//   def spec = suite("Field Spec")(
-//     suite("Field Construction")(
-//       test("A Field can be created from a string and TypeExpr using <:>") {
-//         val actual = "name" <:> Type.variable("String")
-//         assertTrue(
-//           actual == Field("name", Type.variable("String"))
-//         )
-//       },
-//       test("A Field can be created from a string and a Type using as") {
-//         val actual = "name" <:> Type.variable("String")
-//         assertTrue(
-//           actual == Field("name", Type.variable("String"))
-//         )
-//       }
-//     )
-//   )
-// }
+object FieldSpec extends MorphirBaseSpec {
+  def spec = suite("Field Spec")(
+    suite("Field Construction")(
+      test("A Field can be created from a string and TypeExpr using <:>") {
+        val actual = "name" <:> Type.variable("String")
+        assertTrue(
+          actual == Field("name", Type.variable("String"))
+        )
+      },
+      test("A Field can be created from a string and a Type using as") {
+        val actual = "name" <:> Type.variable("String")
+        assertTrue(
+          actual == Field("name", Type.variable("String"))
+        )
+      }
+    )
+  )
+}
