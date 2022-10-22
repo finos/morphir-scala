4f80fb9042b1309a40d796e8418ff5c4b07e07a6
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-10-22 14:13:16 -0400

    CommonSpec tests are passing again

--- morphir/toolkit/core/test/src/org/finos/morphir/ir/sdk/CommonSpec.scala
+++ morphir/toolkit/core/test/src/org/finos/morphir/ir/sdk/CommonSpec.scala
@@ -1,27 +1,26 @@
-// package org.finos.morphir.ir.sdk
+package org.finos.morphir.ir.sdk
 
-// import zio.Scope
-// import org.finos.morphir.ir.Type
-// import org.finos.morphir.ir.{Gens, Path}
-// import org.finos.morphir.testing.MorphirBaseSpec
-// import zio.test._
+import org.finos.morphir.ir.Type
+import org.finos.morphir.ir.{Gens, Path}
+import org.finos.morphir.testing.MorphirBaseSpec
+import zio.test._
 
-// object CommonSpec extends MorphirBaseSpec {
-//   def spec = suite("Common Spec")(
-//     suite("packageName")(
-//       test("should return the expected value") {
-//         assertTrue(Common.packageName.toPath == Path.fromString("Morphir.SDK"))
-//       }
-//     ),
-//     suite("tVar")(
-//       test("should work as expected") {
-//         check(Gens.words) { s =>
-//           val actual = Common.tVar(s)
-//           assertTrue(
-//             actual == Type.variable(s)
-//           )
-//         }
-//       }
-//     )
-//   )
-// }
+object CommonSpec extends MorphirBaseSpec {
+  def spec = suite("Common Spec")(
+    suite("packageName")(
+      test("should return the expected value") {
+        assertTrue(Common.packageName.toPath == Path.fromString("Morphir.SDK"))
+      }
+    ),
+    suite("tVar")(
+      test("should work as expected") {
+        check(Gens.words) { s =>
+          val actual = Common.tVar(s)
+          assertTrue(
+            actual == Type.variable(s)
+          )
+        }
+      }
+    )
+  )
+}
