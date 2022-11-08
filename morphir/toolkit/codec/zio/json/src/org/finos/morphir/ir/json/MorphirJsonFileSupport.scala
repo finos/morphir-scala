a4af78aa5ee2b868a81354990bc3e2b1231c6423
Dmitry Kozinets <dkozinets@yahoo.com>
2022-11-08 16:41:36 -0500

    Adding support to decode and encode different file versions automatically

---
+++ morphir/toolkit/codec/zio/json/src/org/finos/morphir/ir/json/MorphirJsonFileSupport.scala
@@ -0,0 +1,67 @@
+package org.finos.morphir.ir
+package json
+
+import zio._
+import zio.json._
+import zio.json.ast.Json
+import org.finos.morphir.ir.distribution.Distribution
+import scala.annotation.nowarn
+
+trait MorphirJsonFileSupport {
+  implicit val morphirIRVersionDecoder: JsonDecoder[MorphirIRVersion] = JsonDecoder.int.map {
+    case 1 => MorphirIRVersion.V1_0
+    case 2 => MorphirIRVersion.V2_0
+  }
+
+  private def DecodeDistributionWithVersion(version: MorphirIRVersion, distribution: Json) = version match {
+    case MorphirIRVersion.V1_0 =>
+      import org.finos.morphir.ir.json.MorphirJsonDecodingSupportV1._
+      JsonDecoder[Distribution].fromJsonAST(distribution)
+    case MorphirIRVersion.V2_0 =>
+      import org.finos.morphir.ir.json.MorphirJsonDecodingSupport._
+      JsonDecoder[Distribution].fromJsonAST(distribution)
+  }
+
+  implicit def morphirIRFileDecoder: JsonDecoder[MorphirIRFile] =
+    Json.Obj.decoder.mapOrFail[MorphirIRFile] {
+      case Json.Obj(
+            Chunk(
+              "formatVersion" -> formatVersion,
+              "distribution" -> distribution
+            )
+          ) =>
+        for {
+          version <- JsonDecoder[MorphirIRVersion].fromJsonAST(formatVersion)
+          dist    <- DecodeDistributionWithVersion(version, distribution)
+        } yield MorphirIRFile(version, dist)
+      case other =>
+        Left(
+          s"Cannot decode: $other"
+        )
+    }
+
+  implicit val morphirIRVersionEncoder: JsonEncoder[MorphirIRVersion] =
+    JsonEncoder.int.contramap(_.versionNumber.toDouble.toInt)
+
+  implicit def morphirIRFileJsonEncoder: JsonEncoder[MorphirIRFile] =
+    Json.encoder.contramap[MorphirIRFile] { file =>
+      Json.Obj(
+        "formatVersion" -> toJsonAstOrThrow(file.version),
+        "distribution"  -> EncodeDistributionWithVersion(file.version, file.distribution)
+      )
+    }
+
+  private def EncodeDistributionWithVersion(version: MorphirIRVersion, distribution: Distribution) = version match {
+    case MorphirIRVersion.V1_0 =>
+      import org.finos.morphir.ir.json.MorphirJsonEncodingSupportV1._
+      toJsonAstOrThrow(distribution)
+    case MorphirIRVersion.V2_0 =>
+      import org.finos.morphir.ir.json.MorphirJsonEncodingSupport._
+      toJsonAstOrThrow(distribution)
+  }
+
+  private def toJsonAstOrThrow[A](a: A)(implicit encoder: JsonEncoder[A]): Json =
+    a.toJsonAST.toOption.get
+}
+
+object MorphirJsonFileSupport extends MorphirJsonFileSupport
