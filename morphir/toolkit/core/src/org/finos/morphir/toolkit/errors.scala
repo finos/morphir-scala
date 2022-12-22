e3ca333f5a0b8b5276cc65a5056c582252e7f0f3
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2022-12-22 15:05:31 -0500

    Evaluator Changes (#120)
    * Support the concept of pushing and poping a frame onto the evaluationContext
    
    * Evaluate tuples from arities 2 to 22 (#4)
    
    * ifThenElseCase for Evaluator
    
    * Create evaluation engine using ZPure
    
    * Provide evaluation of variables
    
    * More work on evaluation and Kernel
    
    * Add Evaluation stuff
    
    * Working on engine
    
    * Morphir CallStack and stuff
    
    * Comment out failing test
    
    Will be fixed in later work.
    
    * Remove old evaluator (#13)
    
    * Additions to evaluation context, moved stuff to EvaluationEngine.scala
    
    * Removed ZIO-based files, updated TupleAritySuite
    
    * Make tuple arity tests explicit (#14)
    
    * Record case for Evaluator and the supporting MorphirRecord (#15)
    
    * Cleaning up unused Json files (#16)
    
    * fixup: formatAll
    
    Co-authored-by: Justin Corn <60192848+justin-corn@users.noreply.github.com>
    Co-authored-by: Dmitry Kozinets <dkozinets@yahoo.com>
    Co-authored-by: Edward Peters <11288157+edwardpeters@users.noreply.github.com>

---
+++ morphir/toolkit/core/src/org/finos/morphir/toolkit/errors.scala
@@ -0,0 +1,23 @@
+package org.finos.morphir
+package toolkit
+
+import ir.Value.{TypedValue, Value}
+import ir.Type.UType
+import Value.Folder
+import zio.{Tag, ZIO}
+
+sealed abstract class KernelError(message: Option[String])
+    extends Exception(message.orNull)
+    with Product
+    with Serializable
+
+sealed abstract class EvaluationError(message: Option[String]) extends KernelError(message)
+
+object EvaluationError {
+  final case class VariableNotFound(name: Name)
+      extends EvaluationError(Some(s"Evaluation Error: Could not find variable $name"))
+  final case class VariableResolutionError(name: Name, msg: String)
+      extends EvaluationError(Some(s"Evaluation Error: Error occurred while resolving variable $name. $msg"))
+  final case class UnsupportedTupleArity(value: Any, arity: Int)
+      extends EvaluationError(Some(s"Evaluation Error: Unsupported tuple arity of $arity for value: $value"))
+}
---
+++ morphir/toolkit/core/test/src/org/finos/morphir/toolkit/runtime/CallStackSpec.scala
@@ -0,0 +1,21 @@
+package org.finos.morphir
+package toolkit
+package runtime
+
+import org.finos.morphir.testing.MorphirBaseSpec
+import zio.test._
+object CallStackSpec extends MorphirBaseSpec {
+  def spec = suite("CallStack Spec")(
+    test("An empty CallStack can be created") {
+      val sut = CallStack.empty
+      assertTrue(sut.frames.isEmpty)
+    },
+    test("A frame can be pushed onto a CallStack") {
+      val sut         = CallStack.empty
+      val frame       = StackFrame.empty
+      val initialSize = sut.size
+      val actual      = sut.push(frame)
+      assertTrue(actual.size == 1, actual.size == actual.depth)
+    }
+  )
+}
