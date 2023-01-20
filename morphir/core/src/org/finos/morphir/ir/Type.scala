8c5daad37747cbfeb0269095ae65483674d735f8
Dmitry Kozinets <dkozinets@yahoo.com>
2023-01-20 09:47:39 -0500

    finish up Type writers (#132)
    * finish up Type writers
    
    * fix Field and serialization formatting for it

--- morphir/core/src/org/finos/morphir/ir/Type.scala
+++ morphir/core/src/org/finos/morphir/ir/Type.scala
@@ -41,14 +41,14 @@ object Type {
 
     sealed case class ExtensibleRecord[+A](attributes: A, name: Name, fields: List[Field[A]])   extends Type[A]
     sealed case class Function[+A](attributes: A, argumentType: Type[A], returnType: Type[A])   extends Type[A]
-    sealed case class Record[+A](attributes: A, fields: List[Field[Type[A]]])                   extends Type[A]
+    sealed case class Record[+A](attributes: A, fields: List[Field[A]])                         extends Type[A]
     sealed case class Reference[+A](attributes: A, typeName: FQName, typeParams: List[Type[A]]) extends Type[A]
     sealed case class Tuple[+A](attributes: A, elements: List[Type[A]])                         extends Type[A]
     sealed case class Unit[+A](attributes: A)                                                   extends Type[A]
     sealed case class Variable[+A](attributes: A, name: Name)                                   extends Type[A]
   }
 
-  final case class Field[+A](name: String, tpe: Type[A]) {
+  final case class Field[+A](name: Name, tpe: Type[A]) {
     def map[B](f: A => B): Field[B] = Field(name, tpe.map(f))
   }
 
