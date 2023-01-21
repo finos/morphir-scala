ee8a2b155c5e6296eb3b3e98acef2b3fb6fa5d2d
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-01-21 09:11:13 -0500

    Add Pattern

--- morphir/core/src-2/org/finos/morphir/ir/ValueVersionSpecific.scala
+++ morphir/core/src-2/org/finos/morphir/ir/ValueVersionSpecific.scala
@@ -5,6 +5,21 @@ package ir
 import Name.Name
 import Type.Type
 trait ValueVersionSpecific {
+  final type Parameter[+TA, +VA] = (Name, VA, Type[TA])
+  object Parameter {
+    def apply[TA, VA](name: Name, attributes: VA, tpe: Type[TA]): Parameter[TA, VA] = (name, attributes, tpe)
+    def unapply[TA, VA](p: Parameter[TA, VA]): Some[(Name, VA, Type[TA])]           = Some(p)
+
+  }
+
+  implicit class ParameterOps[TA, VA](val p: Parameter[TA, VA]) {
+    def mapValueAttributes[VB](f: VA => VB): Parameter[TA, VB] = (p._1, f(p._2), p._3)
+    def mapTypeAttributes[TB](f: TA => TB): Parameter[TB, VA]  = (p._1, p._2, p._3.map(f))
+    def name: Name                                             = p._1
+    def attributes: VA                                         = p._2
+    def tpe: Type[TA]                                          = p._3
+  }
+
   final type SpecParameter[+A] = (Name, Type[A])
   object SpecParameter {
     def apply[A](name: Name, tpe: Type[A]): SpecParameter[A]   = (name, tpe)
--- morphir/core/src/org/finos/morphir/ir/Value.scala
+++ morphir/core/src/org/finos/morphir/ir/Value.scala
@@ -3,11 +3,14 @@ package morphir
 package ir
 
 import prelude.*
+import FQName.FQName
+import Literal.Literal
 import Name.Name
 import Type.Type
 
 object Value extends ValueVersionSpecific {
 
+  final case class Definition[+TA, +VA](inputTypes: Chunk[Parameter[TA, VA]], outputType: Type[TA], body: Value[TA, VA])
   final case class Specification[+TA](inputs: Chunk[SpecParameter[TA]], output: Type[TA]) { self =>
     def map[TB](f: TA => TB): Specification[TB] =
       Specification(inputs.map(_.map(f)), output.map(f))
@@ -15,4 +18,21 @@ object Value extends ValueVersionSpecific {
   sealed trait Value[+TA, +VA]
   case class Unit[+VA](attributes: VA)                 extends Value[Nothing, VA]
   case class Variable[+VA](attributes: VA, name: Name) extends Value[Nothing, VA]
+
+  sealed trait Pattern[+A] {
+    def attributes: A
+  }
+  object Pattern {
+    final case class WildcardPattern[+A](attributes: A)                                  extends Pattern[A]
+    final case class AsPattern[+A](attributes: A, pattern: Pattern[A], name: Name)       extends Pattern[A]
+    final case class TuplePattern[+A](attributes: A, elementPatterns: Chunk[Pattern[A]]) extends Pattern[A]
+    final case class ConstructorPattern[+A](attributes: A, constructorName: FQName, argumentPatterns: Chunk[Pattern[A]])
+        extends Pattern[A]
+    final case class EmptyListPattern[+A](attributes: A) extends Pattern[A]
+    final case class HeadTailPattern[+A](attributes: A, headPattern: Pattern[A], tailPattern: Pattern[A])
+        extends Pattern[A]
+    final case class LiteralPattern[+A](attributes: A, literal: Literal) extends Pattern[A]
+    final case class UnitPattern[+A](attributes: A)                      extends Pattern[A]
+  }
+
 }
