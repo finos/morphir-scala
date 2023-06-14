d7f0adc7f5c90fef95a0e9f1ad025b9aa17721ec
Alexander Ioffe <deusaquilus@gmail.com>
2023-06-14 18:12:50 -0400

    More basics 3

--- morphir/datamodel/src/org/finos/morphir/datamodel/Data.scala
+++ morphir/datamodel/src/org/finos/morphir/datamodel/Data.scala
@@ -6,6 +6,8 @@ sealed trait Data {
 object Data {
   sealed trait Basic[+A] extends Data
 
+  case class Decimal(value: scala.BigDecimal)      extends Basic[scala.BigDecimal]    { val shape = Concept.Decimal   }
+  case class Integer(value: scala.BigInt)          extends Basic[scala.BigInt]        { val shape = Concept.Integer   }
   case class Int16(value: scala.Short)             extends Basic[Short]               { val shape = Concept.Int16     }
   case class Int32(value: scala.Int)               extends Basic[Int]                 { val shape = Concept.Int32     }
   case class String(value: java.lang.String)       extends Basic[java.lang.String]    { val shape = Concept.String    }
