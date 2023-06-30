b21f5871b9eed51a6186e9082a114a367ea4c654
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-06-30 18:24:25 -0400

    Rename FromData to DataDecoder

---
+++ morphir/datamodel/src/org/finos/morphir/datamodel/DataDecoder.scala
@@ -0,0 +1,35 @@
+package org.finos.morphir.datamodel
+
+trait DataDecoder[A] {
+  import DataDecoder.DecodingError
+  final def apply(value: Data): Either[DecodingError, A] = decode(value)
+  def decode(value: Data): Either[DecodingError, A]
+}
+
+object DataDecoder {
+
+  implicit val boolean: DataDecoder[Boolean] = (value: Data) => value match {
+    case Data.Boolean(value) => Right(value)
+    case _ => Left(DecodingError.InvalidConversion("Boolean"))
+  }
+
+  implicit val byte: DataDecoder[Byte] = (value: Data) => value match {
+    case Data.Byte(value) => Right(value)
+    case _ => Left(DecodingError.InvalidConversion("Byte"))
+  }
+
+  implicit val decimal: DataDecoder[BigDecimal] = (value: Data) => value match {
+    case Data.Decimal(value) => Right(value)
+    case _ => Left(DecodingError.InvalidConversion("BigDecimal"))
+  }
+
+  implicit val string: DataDecoder[String] = (value: Data) => value match {
+    case Data.String(value) => Right(value)
+    case _ => Left(DecodingError.InvalidConversion("String"))
+  }
+
+  sealed abstract class DecodingError(msg:Option[String] = None, cause:Option[Throwable]) extends Exception(msg.orNull, cause.orNull) with Serializable with Product
+  object DecodingError {
+    final case class InvalidConversion(typeName:String) extends DecodingError(Option(s"Conversion to type $typeName is invalid/not possible."), None)
+  }
+}
