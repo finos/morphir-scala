24a846c6275c961fcf0f6fd96ffe88c9c59614e2
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-06-30 18:06:01 -0400

    Added a few additional DataEncoder implementations

--- morphir/datamodel/src/org/finos/morphir/datamodel/DataEncoder.scala
+++ morphir/datamodel/src/org/finos/morphir/datamodel/DataEncoder.scala
@@ -7,11 +7,12 @@ trait DataEncoder[A] {
 object DataEncoder {
   def apply[A](implicit toData: DataEncoder[A]): DataEncoder[A] = toData
 
-  implicit val boolean: DataEncoder[Boolean] = new DataEncoder[Boolean] {
-    override def encode(value: Boolean): Data = Data.Boolean(value)
-  }
 
-  implicit val string: DataEncoder[String] = new DataEncoder[String] {
-    override def encode(value: String): Data = Data.String(value)
-  }
+  implicit val boolean: DataEncoder[Boolean] = (value: Boolean) => Data.Boolean(value)
+
+  implicit val byte:DataEncoder[Byte] = (value: Byte) => Data.Byte(value)
+
+  implicit val decimal: DataEncoder[BigDecimal] = (value: BigDecimal) => Data.Decimal(value)
+
+  implicit val string: DataEncoder[String] = (value: String) => Data.String(value)
 }
