b9cfff818b39f0ddb724c6aab4faa12116e34f6e
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-07-03 17:52:48 -0400

    formatting

--- morphir/datamodel/src/org/finos/morphir/datamodel/DataDecoder.scala
+++ morphir/datamodel/src/org/finos/morphir/datamodel/DataDecoder.scala
@@ -8,28 +8,34 @@ trait DataDecoder[A] {
 
 object DataDecoder {
 
-  implicit val boolean: DataDecoder[Boolean] = (value: Data) => value match {
-    case Data.Boolean(value) => Right(value)
-    case _ => Left(DecodingError.InvalidConversion("Boolean"))
-  }
+  implicit val boolean: DataDecoder[Boolean] = (value: Data) =>
+    value match {
+      case Data.Boolean(value) => Right(value)
+      case _                   => Left(DecodingError.InvalidConversion("Boolean"))
+    }
 
-  implicit val byte: DataDecoder[Byte] = (value: Data) => value match {
-    case Data.Byte(value) => Right(value)
-    case _ => Left(DecodingError.InvalidConversion("Byte"))
-  }
+  implicit val byte: DataDecoder[Byte] = (value: Data) =>
+    value match {
+      case Data.Byte(value) => Right(value)
+      case _                => Left(DecodingError.InvalidConversion("Byte"))
+    }
 
-  implicit val decimal: DataDecoder[BigDecimal] = (value: Data) => value match {
-    case Data.Decimal(value) => Right(value)
-    case _ => Left(DecodingError.InvalidConversion("BigDecimal"))
-  }
+  implicit val decimal: DataDecoder[BigDecimal] = (value: Data) =>
+    value match {
+      case Data.Decimal(value) => Right(value)
+      case _                   => Left(DecodingError.InvalidConversion("BigDecimal"))
+    }
 
-  implicit val string: DataDecoder[String] = (value: Data) => value match {
-    case Data.String(value) => Right(value)
-    case _ => Left(DecodingError.InvalidConversion("String"))
-  }
+  implicit val string: DataDecoder[String] = (value: Data) =>
+    value match {
+      case Data.String(value) => Right(value)
+      case _                  => Left(DecodingError.InvalidConversion("String"))
+    }
 
-  sealed abstract class DecodingError(msg:Option[String] = None, cause:Option[Throwable]) extends Exception(msg.orNull, cause.orNull) with Serializable with Product
+  sealed abstract class DecodingError(msg: Option[String] = None, cause: Option[Throwable])
+      extends Exception(msg.orNull, cause.orNull) with Serializable with Product
   object DecodingError {
-    final case class InvalidConversion(typeName:String) extends DecodingError(Option(s"Conversion to type $typeName is invalid/not possible."), None)
+    final case class InvalidConversion(typeName: String)
+        extends DecodingError(Option(s"Conversion to type $typeName is invalid/not possible."), None)
   }
 }
--- morphir/runtime/src/org/finos/morphir/runtime/MorphirRuntime.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/MorphirRuntime.scala
@@ -4,21 +4,17 @@ import org.finos.morphir.ir.Value.Value
 import org.finos.morphir.datamodel.Data
 import org.finos.morphir.ir.distribution.Distribution
 
-trait MorphirRuntime[F[_], TA,VA] {
-  final def evaluate(entrypoint:Value[TA,VA], params:Value[TA,VA]): F[Data] = 
-    evaluate(applyParams(entrypoint,params))
+trait MorphirRuntime[F[_], TA, VA] {
+  final def evaluate(entrypoint: Value[TA, VA], params: Value[TA, VA]): F[Data] =
+    evaluate(applyParams(entrypoint, params))
 
-  def evaluate(entryPoint:Value[TA,VA]): F[Data]
+  def evaluate(entryPoint: Value[TA, VA]): F[Data]
 
-  def applyParams(entrypoint:Value[TA,VA], params:Value[TA,VA]):Value[TA,VA]
+  def applyParams(entrypoint: Value[TA, VA], params: Value[TA, VA]): Value[TA, VA]
 }
 
 object MorphirRuntime {
-  def simple(distributions:Distribution*):MorphirRuntime[SimpleMorphirRuntime.Result, scala.Unit,UType] =
+  def simple(distributions: Distribution*): MorphirRuntime[SimpleMorphirRuntime.Result, scala.Unit, UType] =
     SimpleMorphirRuntime(distributions.toList)
 
 }
-
-
-
-
