231ac1aea72ad8b22c1545421ac8595d188af395
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-07-06 01:46:23 +0200

    Update scalafmt-core to 3.7.6 (#240)
    * Update scalafmt-core to 3.7.6
    
    * Reformat with scalafmt 3.7.6
    
    Executed command: scalafmt --non-interactive
    
    * Add 'Reformat with scalafmt 3.7.6' to .git-blame-ignore-revs

--- mill-build/src/millbuild/crossplatform/CrossPlatform.scala
+++ mill-build/src/millbuild/crossplatform/CrossPlatform.scala
@@ -10,17 +10,17 @@ import mill.scalanativelib._
 import scala.language.reflectiveCalls
 
 trait CrossPlatform extends Module with DynamicModule { self =>
-  def moduleDeps:Seq[CrossPlatform] = Seq.empty
-  def compiledModuleDeps:Seq[CrossPlatform] = Seq.empty
+  def moduleDeps: Seq[CrossPlatform]         = Seq.empty
+  def compiledModuleDeps: Seq[CrossPlatform] = Seq.empty
 
-  def enableJVM(module:Module):Boolean = true 
-  def enableJS(module:Module):Boolean = true
-  def enableNative(module:Module):Boolean = true
+  def enableJVM(module: Module): Boolean    = true
+  def enableJS(module: Module): Boolean     = true
+  def enableNative(module: Module): Boolean = true
 
-  private def enableModuleCondition(module:Module):Boolean = module match {
-    case _:ScalaNativeModule => enableNative(module)
-    case _:ScalaJSModule => enableJS(module)
-    case _:ScalaModule => enableJVM(module)
+  private def enableModuleCondition(module: Module): Boolean = module match {
+    case _: ScalaNativeModule => enableNative(module)
+    case _: ScalaJSModule     => enableJS(module)
+    case _: ScalaModule       => enableJVM(module)
     case c: Cross[_] =>
       enableModuleCondition(
         c.crossModules.head.asInstanceOf[Module]
@@ -34,30 +34,29 @@ trait CrossPlatform extends Module with DynamicModule { self =>
     super.millModuleDirectChildren.filter(enableModuleCondition)
 
   private type PlatformModule = JavaModule {
-    def platform:Platform
+    def platform: Platform
   }
 
   object AsPlatformModule {
-    def unapply(m:Module):Option[PlatformModule] = m match {
-      case jm:JavaModule => 
+    def unapply(m: Module): Option[PlatformModule] = m match {
+      case jm: JavaModule =>
         jm match {
-          case (m:PlatformModule @unchecked) => Some(m)
-          case _ => None
+          case (m: PlatformModule @unchecked) => Some(m)
+          case _                              => None
         }
       case _ => None
     }
   }
 
-  def childPlatformModules:Seq[PlatformModule] = 
-    millModuleDirectChildren.collect{case (m:PlatformModule @unchecked) => m}
+  def childPlatformModules: Seq[PlatformModule] =
+    millModuleDirectChildren.collect { case (m: PlatformModule @unchecked) => m }
 
-  def targetPlatforms:T[Seq[Platform]] = T { childPlatformModules.map(_.platform) }
+  def targetPlatforms: T[Seq[Platform]] = T { childPlatformModules.map(_.platform) }
 
   /// Try and resolve to an actual platform specific module if it exists (not expecting multiple but in theory it is possible)
-  def childPlatformModules(platform:Platform):Seq[PlatformModule] = 
-    childPlatformModules.filter{
-      case m:CrossPlatformScalaModule => m.platform == platform
-      case AsPlatformModule(m) => m.platform == platform
+  def childPlatformModules(platform: Platform): Seq[PlatformModule] =
+    childPlatformModules.filter {
+      case m: CrossPlatformScalaModule => m.platform == platform
+      case AsPlatformModule(m)         => m.platform == platform
     }
 }
-
--- morphir/build/integration/mill/morphir-elm/src/org/finos/morphir/mill/elm/MorphirElmModule.scala
+++ morphir/build/integration/mill/morphir-elm/src/org/finos/morphir/mill/elm/MorphirElmModule.scala
@@ -5,9 +5,9 @@ trait MorphirElmModule extends Module {
   def make: T[PathRef] = T {
 
     val outputFolder = T.ctx().dest
-    val path = outputFolder / "morphir-ir.json"    
-    os.makeDir.all(outputFolder)    
+    val path         = outputFolder / "morphir-ir.json"
+    os.makeDir.all(outputFolder)
     os.write.over(path, "{}")
     PathRef(path)
-  }  
+  }
 }
