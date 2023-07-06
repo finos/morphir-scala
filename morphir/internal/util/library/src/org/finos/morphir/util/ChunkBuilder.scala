738cf2d720712efe3052c745d1067a5145aee466
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-07-06 02:47:15 -0400

    Move Chunk and NonEmptyChunk to org.finos.morphir.util

--- morphir/internal/util/library/src/org/finos/morphir/util/ChunkBuilder.scala
+++ morphir/internal/util/library/src/org/finos/morphir/util/ChunkBuilder.scala
@@ -6,7 +6,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  */
-package org.finos.morphir.internal.util
+package org.finos.morphir.util
 
 /**
  * Ported from ZIO: https://github.com/zio/zio/blob/series/2.x/core/shared/src/main/scala-2.13+/zio/ChunkBuilder.scala
