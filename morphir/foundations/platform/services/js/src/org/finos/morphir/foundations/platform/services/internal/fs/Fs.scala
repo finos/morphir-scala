85e9f50e23b66a0e6b214e43e2826d157ca388ce
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-07-13 10:08:38 -0400

    Add API for making a temp directory for Node

--- morphir/foundations/platform/services/js/src/org/finos/morphir/foundations/platform/services/internal/fs/Fs.scala
+++ morphir/foundations/platform/services/js/src/org/finos/morphir/foundations/platform/services/internal/fs/Fs.scala
@@ -6,8 +6,25 @@ import scala.scalajs.js.annotation.JSImport
 @js.native
 trait Fs extends js.Object {
   def existsSync(path: Path): Boolean = js.native
+  def mkdtempSync(prefix: String): String = js.native
+  def mkdtempSync(prefix: String, options:FsMkTempDirOptions): String = js.native
 }
 
 @js.native
 @JSImport("fs", JSImport.Namespace)
 object Fs extends Fs {}
+
+trait FsMkTempDirOptions extends js.Object {  
+  val encoding: js.UndefOr[String] = js.undefined
+}
+
+object FsMkTempDirOptions {
+  def apply(
+      encoding: js.UndefOr[String] = js.undefined
+  ): FsMkTempDirOptions = {
+    val _obj$ = js.Dynamic.literal(
+    )
+    encoding.foreach(_v => _obj$.updateDynamic("encoding")(_v.asInstanceOf[js.Any]))
+    _obj$.asInstanceOf[FsMkTempDirOptions]
+  }
+}
