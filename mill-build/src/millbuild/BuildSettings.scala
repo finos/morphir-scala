b71a406fb3ae2045949250ff8d393caba2d9f183
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-07-14 14:08:28 -0400

    Allow to take settings from various levels of Config

--- mill-build/src/millbuild/BuildSettings.scala
+++ mill-build/src/millbuild/BuildSettings.scala
@@ -4,6 +4,8 @@ import zio.config._
 import zio.config.magnolia.deriveConfig
 import zio.config.typesafe._
 import zio.config.yaml._
+import com.typesafe.config.ConfigFactory
+import zio.Config
 
 final case class BuildSettings(
     jvm: JvmBuildSettings = JvmBuildSettings(),
@@ -12,19 +14,26 @@ final case class BuildSettings(
 )
 
 object BuildSettings {
-  lazy val default: BuildSettings = BuildSettings()
+  val config: Config[BuildSettings] = deriveConfig[BuildSettings]
+  lazy val default: BuildSettings   = BuildSettings()
 
   implicit lazy val rw: upickle.default.ReadWriter[BuildSettings] = upickle.default.macroRW
 
-  lazy val buildSettingsConfig = deriveConfig[BuildSettings]
-
-  lazy val buildUserHoconFileSource =
+  lazy val buildUserHoconFileConfigProvider: ConfigProvider =
     ConfigProvider.fromHoconFile((os.pwd / "build.user.conf").toIO)
+
+  lazy val buildEnvConfigProvider: ConfigProvider =
+    ConfigProvider.envProvider.nested("morphir_build")
+
+  lazy val propertiesFileConfigProvider: ConfigProvider =
+    ConfigProvider.propsProvider.nested("morphir.build")
+
+    // .fromPropertiesFile((os.pwd / "build.user.properties").toIO)
 
   // lazy val buildUserYamlFileSource =
   //   ConfigProvider.fromYamlPath((os.pwd / "build.user.yaml").wrapped)
 
-  lazy val hoconFallbackSource = ConfigProvider.fromHoconString(
+  lazy val hoconFallbackSource: ConfigProvider = ConfigProvider.fromHoconString(
     """
       |jvm {
       |  enable = true
@@ -46,13 +55,17 @@ object BuildSettings {
 
   def loadSettings() =
     read(
-      buildSettingsConfig from (buildUserHoconFileSource /* orElse buildUserYamlFileSource */ orElse hoconFallbackSource)
+      BuildSettings.config from (
+        buildEnvConfigProvider orElse propertiesFileConfigProvider orElse
+          buildUserHoconFileConfigProvider /* orElse buildUserYamlFileSource  orElse hoconFallbackSource*/
+      )
     )
 
 }
 
 final case class JvmBuildSettings(enable: Boolean = true)
 object JvmBuildSettings {
+  val config                         = deriveConfig[JvmBuildSettings]
   lazy val default: JvmBuildSettings = JvmBuildSettings()
 
   implicit lazy val rw: upickle.default.ReadWriter[JvmBuildSettings] = upickle.default.macroRW
@@ -60,6 +73,7 @@ object JvmBuildSettings {
 
 final case class ScalaJsBuildSettings(enable: Boolean = true)
 object ScalaJsBuildSettings {
+  val config                             = deriveConfig[ScalaJsBuildSettings]
   lazy val default: ScalaJsBuildSettings = ScalaJsBuildSettings()
 
   implicit lazy val rw: upickle.default.ReadWriter[ScalaJsBuildSettings] = upickle.default.macroRW
@@ -67,6 +81,7 @@ object ScalaJsBuildSettings {
 
 final case class ScalaNativeBuildSettings(enable: Boolean = true)
 object ScalaNativeBuildSettings {
+  val config                                 = deriveConfig[ScalaNativeBuildSettings]
   lazy val default: ScalaNativeBuildSettings = ScalaNativeBuildSettings()
 
   implicit lazy val rw: upickle.default.ReadWriter[ScalaNativeBuildSettings] = upickle.default.macroRW
