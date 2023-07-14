7b8b9cb0bb022e56d5e73883af83f2f574ba06e0
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-07-14 18:21:15 -0400

    Include namespacing

--- morphir/datamodel/src/org/finos/morphir/datamodel/namespacing.scala
+++ morphir/datamodel/src/org/finos/morphir/datamodel/namespacing.scala
@@ -2,6 +2,9 @@ package org.finos.morphir.datamodel
 import org.finos.morphir.foundations.*
 
 object namespacing {
+
+  def localName(name: String): LocalName = LocalName(name)
+
   type NamespaceSegment = NamespaceSegment.Type
   object NamespaceSegment extends Subtype[String]
   implicit class NamespaceSegmentOps(val self: NamespaceSegment) extends AnyVal {
@@ -12,7 +15,29 @@ object namespacing {
   type Namespace = Namespace.Type
   object Namespace extends Newtype[Chunk[NamespaceSegment]] {
     lazy val root: Namespace = Namespace(Chunk.empty)
+
+    def fromIterable(segments: Iterable[NamespaceSegment]): Namespace =
+      Namespace(Chunk.fromIterable(segments))
+
+    implicit class NamespaceOps(val self: Namespace) extends AnyVal {
+      def segments: Chunk[NamespaceSegment]       = unwrap(self)
+      def /(segment: NamespaceSegment): Namespace = Namespace(unwrap(self) :+ segment)
+      def /(namespace: Namespace): Namespace      = Namespace(unwrap(self) ++ unwrap(namespace))
+      // def /(name: String): Namespace = Namespace(unwrap(self) :+ NamespaceSegment(name))
+      // def /(names: Iterable[String]): Namespace = Namespace(unwrap(self) ++ names.map(NamespaceSegment(_)))
+      // def /(names: String*): Namespace = /(names)
+      // def /(names: Chunk[String]): Namespace = Namespace(unwrap(self) ++ names.map(NamespaceSegment(_)))
+    }
   }
 
-  final case class Namespaced[+A](namespace: Namespace, value: A)
+  type LocalName = LocalName.Type
+  object LocalName extends Subtype[String] {
+    implicit class LocalNameOps(val self: LocalName) extends AnyVal {
+      def value: String                                    = unwrap(self)
+      def toQualified(namespace: Namespace): QualifiedName = QualifiedName(namespace, self)
+    }
+
+  }
+
+  final case class QualifiedName(namespace: Namespace, localName: LocalName)
 }
--- morphir/datamodel/test/src/org/finos/morphir/datamodel/NamespacingTestSuite.scala
+++ morphir/datamodel/test/src/org/finos/morphir/datamodel/NamespacingTestSuite.scala
@@ -1,4 +1,15 @@
 package org.finos.morphir.datamodel
 import org.finos.morphir.testing.munit.*
+import org.finos.morphir.datamodel.namespacing.*
+class NamespacingTestSuite extends MorphirTestSuite {
+  describe("When qualifying a LocalName") {
+    test("Given a LocalName and a Namespace, then a QualifiedName is returned") {
+      val localName  = LocalName("foo")
+      val namespace  = Namespace.root / NamespaceSegment("bar")
+      val qName      = localName.toQualified(namespace)
+      val expectedQN = QualifiedName(namespace, localName)
+      assertEquals(qName, expectedQN)
+    }
+  }
 
-class NamespacingTestSuite extends MorphirTestSuite {}
+}
