789927b307ac205331f5d4e0f694d1c7bc2071dd
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-07-18 18:15:44 -0400

    Free implementation refinement

--- morphir/foundations/test/src/org/finos/morphir/foundations/capabilities/free/FreeSuite.scala
+++ morphir/foundations/test/src/org/finos/morphir/foundations/capabilities/free/FreeSuite.scala
@@ -6,22 +6,42 @@ class FreeSuite extends MorphirTestSuite {
     test("should be able to create a Free dsl") {
       import org.finos.morphir.foundations.capabilities.free.example.counter
       import counter.dsl.*
-      // val program = for {
-      //   original   <- get()
-      //   _          <- increment(1)
-      //   _          <- increment(2)
-      //   _          <- increment(3)
-      //   checkpoint <- get()
-      //   _          <- decrement(1)
-      //   _          <- decrement(2)
-      //   _          <- decrement(3)
-      //   finalVal   <- get()
-      // } yield (original, checkpoint, finalVal)
-      val program = get() // .map(a => (a, a, a))
+      val program = for {
+        original   <- get()
+        _          <- increment(1)
+        _          <- increment(2)
+        _          <- increment(3)
+        checkpoint <- get()
+        _          <- decrement(1)
+        _          <- decrement(2)
+        _          <- decrement(3)
+        finalVal   <- get()
+      } yield (original, checkpoint, finalVal)
+      // val program = get() // .map(a => (a, a, a))
       pprint.pprintln(program)
       val result = program.unsafeInterpret(counter.interpreters.unsafeInterpreter(0))
-      // expectEquals(result, Right((0, 6, 0)))
-      expectEquals(result, Right(0))
+      expectEquals(result, Right((0, 6, 0)))
+      // expectEquals(result, Right(0))
+    }
+    test("should be able to create a Free dsl") {
+      import org.finos.morphir.foundations.capabilities.free.example.counter
+      import counter.dsl.*
+      val program = for {
+        original   <- get()
+        _          <- increment(1)
+        _          <- increment(2)
+        _          <- increment(3)
+        checkpoint <- get()
+        _          <- decrement(1)
+        _          <- decrement(2)
+        _          <- decrement(3)
+        finalVal   <- get()
+      } yield (original, checkpoint, finalVal)
+      // val program = get() // .map(a => (a, a, a))
+      pprint.pprintln(program)
+      val result = program.unsafeInterpret(counter.interpreters.unsafeInterpreter(0))
+      expectEquals(result, Right((0, 6, 0)))
+      // expectEquals(result, Right(0))
     }
   }
 }
