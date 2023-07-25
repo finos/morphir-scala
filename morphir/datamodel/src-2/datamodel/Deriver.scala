eb079e5b11e3c0cccecfa2d104b969f9dff788cb
Alexander Ioffe <deusaquilus@gmail.com>
2023-07-25 18:25:07 -0400

    Fix stub

--- morphir/datamodel/src-2/datamodel/Deriver.scala
+++ morphir/datamodel/src-2/datamodel/Deriver.scala
@@ -1,7 +1,8 @@
 package org.finos.morphir.datamodel
 
 // Stub so Scala 2 can compile org.finos.morphir.datamodel package since it requires the Deriver trait
-trait Deriver[T] {
-  def derive(value: T): Data
+trait DataEncoder[T] {
+  final def apply(value: T): Data = encode(value)
+  def encode(value: T): Data
   def concept: Concept
 }
