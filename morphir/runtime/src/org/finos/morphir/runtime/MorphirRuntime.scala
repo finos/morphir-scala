d2320bd58a7fc3906f781f3840ca96e324fd37cc
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-08-02 22:36:02 -0400

    Formatting

--- morphir/runtime/src/org/finos/morphir/runtime/MorphirRuntime.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/MorphirRuntime.scala
@@ -24,7 +24,7 @@ trait MorphirRuntime[TA, VA] {
 object MorphirRuntime extends MorphirRuntimePlatformSpecific {
   type RT[+E, +A] = ZPure[Nothing, Unit, Unit, Any, E, A]
   val RT: zio.prelude.fx.ZPure.type = zio.prelude.fx.ZPure
-  
+
   def quick(distribution: Distribution): MorphirRuntime[scala.Unit, UType] =
     QuickMorphirRuntime.fromDistribution(distribution)
 }
