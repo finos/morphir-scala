da9e060a3e2f4ad8b8da9890bdddbd88236b6d26
DmitryKozinets <dkozinets@yahoo.com>
2023-08-04 19:52:54 -0400

    MorphirJsonEncodingSupportV2

--- morphir/toolkit/codec/zio/json/src/org/finos/morphir/ir/json/MorphirJsonEncodingSupportV2.scala
+++ morphir/toolkit/codec/zio/json/src/org/finos/morphir/ir/json/MorphirJsonEncodingSupportV2.scala
@@ -259,7 +259,7 @@ trait MorphirJsonEncodingSupportV2 extends JsonEncodingHelpers {
     Json.encoder.contramap[ValueSpecification[A]] { specification =>
       Json.Obj(
         "inputs"  -> toJsonAstOrThrow(specification.inputs),
-        "outputs" -> toJsonAstOrThrow(specification.output)
+        "output" -> toJsonAstOrThrow(specification.output)
       )
     }
 
