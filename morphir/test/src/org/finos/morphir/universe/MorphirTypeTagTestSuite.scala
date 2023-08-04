ff25e1a06347f936d1851a2ca9e2833cfe5336c3
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-08-03 22:03:08 -0400

    Fix issue with import

--- morphir/test/src/org/finos/morphir/universe/MorphirTypeTagTestSuite.scala
+++ morphir/test/src/org/finos/morphir/universe/MorphirTypeTagTestSuite.scala
@@ -6,7 +6,6 @@ class MorphirTypeTagTestSuite extends MorphirTestSuite {
   describe("MorphirTypeTag") {
     describe("Instances") {
       describe("for org.finos.morphir.sdk.dsl") {
-        import org.finos.morphir.sdk.dsl.*
         // test("An instance for sdk.dsl.Basics.Int should exist") {
         //   val tag = MorphirTypeTag[Basics.Int]
         //   assert(tag != null)
