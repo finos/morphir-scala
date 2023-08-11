84dd853138562309edf8a2eb08849fa60773e890
Alexander Ioffe <deusaquilus@gmail.com>
2023-08-11 14:04:38 -0400

    Single-Constructor Record Enums

--- morphir/test/src-3/org/finos/morphir/datamodel/ToDataEnumsSimple.scala
+++ morphir/test/src-3/org/finos/morphir/datamodel/ToDataEnumsSimple.scala
@@ -20,6 +20,22 @@ object EnumData4 {
   val deriver = Deriver.gen[Foo]
 }
 
+object EnumData5 {
+  import EnumGns._
+  implicit val gnsImpl: GlobalDatamodelContext = new GlobalDatamodelContext {
+    def value                    = gns
+    override def enumTranslation = EnumTranslation.SingleFieldWithRecord
+  }
+
+  sealed trait Foo
+  object Foo {
+    case object Bar               extends Foo
+    case class Baz(value: String) extends Foo
+  }
+
+  val deriver = Deriver.gen[Foo]
+}
+
 class ToDataEnumsSimple extends munit.FunSuite {
   import EnumGns._
 
@@ -40,5 +56,30 @@ class ToDataEnumsSimple extends munit.FunSuite {
     interceptMessage[IllegalArgumentException]("The value `null` is not an instance of the needed enum class Foo") {
       deriver.derive(null)
     }
+  }
+
+  val concept2 =
+    Enum(
+      gns % ("Foo"),
+      Enum.Case(l"Bar"),
+      Enum.Case(Label("Baz"), EnumLabel.Empty -> Concept.Struct(Label("value") -> Concept.String))
+    )
+
+  test("Enum Data 5 - Concept") {
+    import EnumData5._
+    assertEquals(deriver.concept, concept2)
+  }
+
+  test("Enum Data 5 - NoVals") {
+    import EnumData5._
+    assertEquals(deriver.derive(Foo.Bar), Case(List(), "Bar", concept2))
+  }
+
+  test("Enum Data 5 - Value") {
+    import EnumData5._
+    assertEquals(
+      deriver.derive(Foo.Baz("something")),
+      Case(EnumLabel.Empty -> Data.Struct(l"value" -> Data.String("something")))("Baz", concept2)
+    )
   }
 }
