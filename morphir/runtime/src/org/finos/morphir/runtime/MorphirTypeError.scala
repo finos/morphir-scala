a6a666e5f851e902adee98d3b360e95ac39a3196
Edward Peters <edwardstuyvesantpeters@gmail.com>
2023-08-15 11:00:44 -0700

    Incremental

--- morphir/runtime/src/org/finos/morphir/runtime/MorphirTypeError.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/MorphirTypeError.scala
@@ -19,7 +19,7 @@ object MorphirTypeError {
   def succinct[TA, VA](value: Value[TA, VA]): String = Succinct.Value(value)
   def succinct[TA](tpe: Type[TA]): String            = Succinct.Type(tpe)
 
-  def succinct[TA](spec: T.Specification[TA]): String = Succinct.Type(spec.getClass.simpleName())
+  def succinct[TA](spec: T.Specification[TA]): String = Succinct.TypeSpec(spec)
   case class TypesMismatch(tpe1: UType, tpe2: UType, msg: String)
       extends MorphirTypeError(s"$msg: ${succinct(tpe1)} vs ${succinct(tpe2)}")
   case class ArgumentDoesNotMatchParameter(arg: TypedValue, param: UType) extends MorphirTypeError(
