c00fe9ac84c85013232e8a09833d113a9fdcb690
Edward Peters <edwardstuyvesantpeters@gmail.com>
2023-08-14 17:28:57 -0700

    Incremental

--- morphir/runtime/src/org/finos/morphir/runtime/MorphirTypeError.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/MorphirTypeError.scala
@@ -13,14 +13,13 @@ import zio.Chunk
 
 trait MorphirTypeError(msg: String) extends Exception(msg)
 object MorphirTypeError {
-  case class TypesMismatch(tpe1: UType, tpe2: UType)                           extends GoodTypeError("Todo")
-  case class ArgumentDoesNotMatchParameter(arg: TypedValue, param: TypedValue) extends GoodTypeError("Todo")
-  case class ImproperType(tpe: UType, message: String)                         extends GoodTypeError("Todo")
-  case class TypeMissing(tpe: UType)                                           extends GoodTypeError("Todo")
-  case class ValueMissing(value: TypedValue)                                   extends GoodTypeError("Todo")
-  case class ConstructorMissing(fqn: FQName, tpe: UType)                       extends GoodTypeError("Todo")
-  case class ModuleMissing(modName: ModuleName)                                extends GoodTypeError("Todo")
-  case class PackageMissing(pckName: PackageName)                              extends GoodTypeError("Todo")
-  case class Unimplemented(s: String)                                          extends GoodTypeError(s)
-
+  case class TypesMismatch(tpe1: UType, tpe2: UType)                           extends MorphirTypeError("Todo")
+  case class ArgumentDoesNotMatchParameter(arg: TypedValue, param: TypedValue) extends MorphirTypeError("Todo")
+  case class ImproperType(tpe: UType, message: String)                         extends MorphirTypeError("Todo")
+  case class TypeMissing(tpe: UType)                                           extends MorphirTypeError("Todo")
+  case class ValueMissing(value: TypedValue)                                   extends MorphirTypeError("Todo")
+  case class ConstructorMissing(fqn: FQName, tpe: UType)                       extends MorphirTypeError("Todo")
+  case class ModuleMissing(modName: ModuleName)                                extends MorphirTypeError("Todo")
+  case class PackageMissing(pckName: PackageName)                              extends MorphirTypeError("Todo")
+  case class Unimplemented(s: String)                                          extends MorphirTypeError(s)
 }
