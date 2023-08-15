0e85bd7a0e343b7995e546c11e8e776710f7e3b8
Edward Peters <edwardstuyvesantpeters@gmail.com>
2023-08-15 06:39:45 -0700

    Incremental

--- morphir/runtime/src/org/finos/morphir/runtime/MorphirTypeError.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/MorphirTypeError.scala
@@ -17,9 +17,9 @@ abstract class MorphirTypeError(msg: String) extends Exception(msg){
 object MorphirTypeError {
   def succinct[TA, VA](value : Value[TA, VA]) : String= Succinct.Value(value)
   def succinct[TA](tpe : Type[TA]) : String = Succinct.Type(tpe)
-  case class TypesMismatch(tpe1: UType, tpe2: UType, msg : String)                           extends MorphirTypeError(s"$msg: ${Succinct.Type(tp1)} vs ${Sucinct.Type(tpe2)}")
-  case class ArgumentDoesNotMatchParameter(arg: TypedValue, param: UType) extends MorphirTypeError(s"Argument ${Succinct.Value(arg)}  of type ${Succinct.Type(arg.attributes)} does not match parameter ${Succinct.Type(param)}")
-  case class ApplyToNonFunction(arg : TypedValue, nonFunction : TypedValue) extneds MorphirTypeErr(s"Tried to apply ${Succinct.}")
+  case class TypesMismatch(tpe1: UType, tpe2: UType, msg : String)                           extends MorphirTypeError(s"$msg: succinct(tp1)} vs ${succinct(tpe2)}")
+  case class ArgumentDoesNotMatchParameter(arg: TypedValue, param: UType) extends MorphirTypeError(s"Argument ${succinct(arg)}  of type ${succinct(arg.attributes)} does not match parameter succinct(param)}")
+  case class ApplyToNonFunction(arg : TypedValue, nonFunction : TypedValue) extneds MorphirTypeErr(s"Tried to apply ${sucinct(arg)} to ${succinct(nonFunction)} of type ${succinct(nonFunction.attributes)}, which is not a function")
   case class ImproperType(tpe: UType, message: String)                         extends MorphirTypeError("Todo")
   case class CannotDealias(err : LookupError, msg : String = "Cannot dealias type")                                  extends MorphirTypeError(s"$msg: ${err.getMsg}")
   case class ValueMissing(value: TypedValue)                                   extends MorphirTypeError("Todo")
