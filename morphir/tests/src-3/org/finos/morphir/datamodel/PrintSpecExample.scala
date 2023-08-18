256087d22ae732b218895157f5cc24cca7958bbb
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-08-18 09:39:22 -0400

    Moving Deriver tests

--- morphir/tests/src-3/org/finos/morphir/datamodel/PrintSpecExample.scala
+++ morphir/tests/src-3/org/finos/morphir/datamodel/PrintSpecExample.scala
@@ -1,11 +1,14 @@
 package org.finos.morphir.datamodel
 
-import org.finos.morphir.datamodel.{*, given}
+import org.finos.morphir.naming._
 
+/**
+ * PrintSpec allows you to print out an "Elmish" representation of a Morphir concept. The focus of `PrintSpec` is to
+ * allow you to see what the type specification/definition of a concept would look like in Elm.
+ */
 object PrintSpecExample {
-  import EnumGns._
   implicit val gnsImpl: GlobalDatamodelContext = new GlobalDatamodelContext {
-    def value = gns
+    def value = root / "morphir" % "test" / "PrintSpecExample"
   }
 
   case class Person(name: String, age: Int)
@@ -15,6 +18,7 @@ object PrintSpecExample {
   case class Baz(value: String, person: Person) extends Foo
 
   def main(args: Array[String]): Unit = {
+    // Obtain an instance of a Deriver for the Foo type
     val derive = Deriver.gen[Foo]
     println(PrintSpec.of(derive.concept))
   }
