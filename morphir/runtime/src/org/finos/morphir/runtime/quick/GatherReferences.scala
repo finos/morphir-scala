62b8b222a001427ecc63d54b4707e022c96f0ba6
Edward Peters <edwardstuyvesantpeters@gmail.com>
2023-08-22 09:13:54 -0700

    Formatting, disable tests

--- morphir/runtime/src/org/finos/morphir/runtime/quick/GatherReferences.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/quick/GatherReferences.scala
@@ -13,29 +13,30 @@ import zio.Chunk
 
 object GatherReferences {
   type TypedValue = Value[Unit, UType]
-  //Gather references from distribution*
-  //Also from Store? (Yeah, redundancy is okay, and there are
-  //Helper: Recursively explore value
-  //Diff vs. Store
-  def fromStore(store : Store[Unit, UType]) : ReferenceSet = {
-    store.definitions.map{case (name, value) => value match {
-      case SDKValue.SDKValueDefinition(definition) => loop(definition.body).withDefinition(name) //TODO: Types!
-      case _ => ReferenceSet.empty.withDefinition(name)
-    }}.foldLeft(ReferenceSet.empty)((acc, next) => acc ++ next) ++
+  // Gather references from distribution*
+  // Also from Store? (Yeah, redundancy is okay, and there are
+  // Helper: Recursively explore value
+  // Diff vs. Store
+  def fromStore(store: Store[Unit, UType]): ReferenceSet =
+    store.definitions.map { case (name, value) =>
+      value match {
+        case SDKValue.SDKValueDefinition(definition) => loop(definition.body).withDefinition(name) // TODO: Types!
+        case _                                       => ReferenceSet.empty.withDefinition(name)
+      }
+    }.foldLeft(ReferenceSet.empty)((acc, next) => acc ++ next) ++
       store.ctors.keys.foldLeft(ReferenceSet.empty)((acc, next) => acc.withConstructor(next))
-  }
 
-  def fromEntrySet(entrySet : ReferenceSet, dists : Distribution*) : ReferenceSet = {
+  def fromEntrySet(entrySet: ReferenceSet, dists: Distribution*): ReferenceSet = {
     val mapped = dists.map(dist => (dist.asInstanceOf[Library].packageName, dist)).toMap
-    def f(known : Set[FQName], ref : FQName) : Set[FQName] = {
-      //if (depth > 100) throw new Exception(s"Still recursing on $next with known values ${known.toList.mkString("\n")}")
+    def f(known: Set[FQName], ref: FQName): Set[FQName] = {
+      // if (depth > 100) throw new Exception(s"Still recursing on $next with known values ${known.toList.mkString("\n")}")
       val (pkg, mod, loc) = (ref.pack, ref.getModulePath, ref.localName)
-      val qName = QName(mod, loc)
+      val qName           = QName(mod, loc)
       mapped.get(pkg) match {
         case Some(dist) =>
           val definition = dist.asInstanceOf[Library].lookupValueDefinition(qName).get
           val discovered = loop(definition.body).definitions
-          val newbs = discovered.diff(known)
+          val newbs      = discovered.diff(known)
           println(s"Exploring $ref found $discovered")
           newbs.foldLeft(known ++ discovered)((acc, newb) => acc ++ f(acc, newb))
         case None => known + ref
@@ -44,22 +45,24 @@ object GatherReferences {
     val found = entrySet.definitions.foldLeft(entrySet.definitions)((acc, newb) => acc ++ f(acc, newb))
     ReferenceSet(entrySet.definitions ++ found, Set(), Set())
   }
-  def fromDistributions(dists : Distribution*): ReferenceSet = {
-    dists.foldLeft(ReferenceSet.empty)((acc : ReferenceSet, dist : Distribution) => acc ++ (dist match{
-      case l : Library => fromLibrary(l)
-    }))
-  }
-  def fromLibrary(lib : Library) : ReferenceSet = {
+  def fromDistributions(dists: Distribution*): ReferenceSet =
+    dists.foldLeft(ReferenceSet.empty)((acc: ReferenceSet, dist: Distribution) =>
+      acc ++ (dist match {
+        case l: Library => fromLibrary(l)
+      })
+    )
+  def fromLibrary(lib: Library): ReferenceSet = {
     val packageName = lib.packageName
-    val valueReferences : List[ReferenceSet] = lib.packageDef.modules.toList.flatMap { case (moduleName, accessControlledModule) =>
-      accessControlledModule.value.values.map {
-        case (localName, accessControlledValue) =>
-          val fqn = FQName(packageName, moduleName, localName)
-          val definition = accessControlledValue.value.value
-          loop(definition.body).withDefinition(fqn)
+    val valueReferences: List[ReferenceSet] =
+      lib.packageDef.modules.toList.flatMap { case (moduleName, accessControlledModule) =>
+        accessControlledModule.value.values.map {
+          case (localName, accessControlledValue) =>
+            val fqn        = FQName(packageName, moduleName, localName)
+            val definition = accessControlledValue.value.value
+            loop(definition.body).withDefinition(fqn)
+        }
       }
-    }
-    val ctorReferences : List[ReferenceSet]=
+    val ctorReferences: List[ReferenceSet] =
       lib.packageDef.modules.toList.flatMap { case (moduleName, accessControlledModule) =>
         accessControlledModule.value.types.flatMap {
           case (localName, accessControlledType) =>
@@ -80,78 +83,81 @@ object GatherReferences {
   }
 
   case class ReferenceSet(
-                         definitions : Set[FQName],
-                         constructors : Set[FQName],
-                         types : Set[FQName]
-                         ){
-    def ++ (other : ReferenceSet) : ReferenceSet = {
+      definitions: Set[FQName],
+      constructors: Set[FQName],
+      types: Set[FQName]
+  ) {
+    def ++(other: ReferenceSet): ReferenceSet =
       ReferenceSet(
         definitions ++ other.definitions,
         constructors ++ other.constructors,
         types ++ other.types
       )
-    }
-    def withConstructor(ctor : FQName) = {
+    def withConstructor(ctor: FQName) =
       ReferenceSet(
         definitions,
         constructors + ctor,
         types
       )
-    }
 
-    def withDefinition(definition: FQName) = {
+    def withDefinition(definition: FQName) =
       ReferenceSet(
         definitions + definition,
         constructors,
         types
       )
-    }
   }
-  object ReferenceSet{
+  object ReferenceSet {
     val empty = ReferenceSet(Set(), Set(), Set())
   }
 
   def loop(ir: TypedValue): ReferenceSet = {
     val empty = ReferenceSet.empty;
 
-    def fold(stuff: Iterable[TypedValue]): ReferenceSet = stuff.foldLeft(empty) { case (acc, next) => acc ++ loop(next) }
+    def fold(stuff: Iterable[TypedValue]): ReferenceSet = stuff.foldLeft(empty) { case (acc, next) =>
+      acc ++ loop(next)
+    }
 
     ir match {
-      case Value.Literal(_, lit) => empty
+      case Value.Literal(_, lit)              => empty
       case Value.Apply(_, function, argument) => loop(function) ++ loop(argument)
       case Value.Destructure(_, pattern, valueToDestruct, inValue) =>
         loop(valueToDestruct) ++ loop(inValue) ++ patternLoop(pattern)
-      case Value.Constructor(_, fqn) => empty.withConstructor(fqn)
+      case Value.Constructor(_, fqn)      => empty.withConstructor(fqn)
       case Value.Field(_, recordValue, _) => loop(recordValue)
-      case Value.FieldFunction(_, name) => empty
+      case Value.FieldFunction(_, name)   => empty
       case Value.IfThenElse(_, condition, thenValue, elseValue) =>
         loop(condition) ++ loop(thenValue) ++ loop(elseValue)
       case Value.Lambda(_, pattern, body) => patternLoop(pattern) ++ loop(body)
       case Value.LetDefinition(_, _, definition, inValue) =>
         loop(definition.body) ++ loop(inValue)
       case Value.LetRecursion(_, definitions, inValue) => fold(definitions.map(_._2.body)) ++ loop(inValue)
-      case Value.List(_, elements) => fold(elements)
-      case Value.PatternMatch(_, value, cases) => loop(value) ++ cases.foldLeft(empty) { case (acc, (pattern, value)) => acc ++ loop(value) ++ patternLoop(pattern) }
-      case Value.Record(_, fields) => fold(fields.map(_._2))
-      case Value.Reference(_, name) => empty.withDefinition(name)
-      case Value.Tuple(_, elements) => fold(elements)
-      case Value.Unit(_) => empty
+      case Value.List(_, elements)                     => fold(elements)
+      case Value.PatternMatch(_, value, cases) => loop(value) ++ cases.foldLeft(empty) { case (acc, (pattern, value)) =>
+          acc ++ loop(value) ++ patternLoop(pattern)
+        }
+      case Value.Record(_, fields)                      => fold(fields.map(_._2))
+      case Value.Reference(_, name)                     => empty.withDefinition(name)
+      case Value.Tuple(_, elements)                     => fold(elements)
+      case Value.Unit(_)                                => empty
       case Value.UpdateRecord(_, valueToUpdate, fields) => loop(valueToUpdate) ++ fold(fields.map(_._2))
-      case Value.Variable(_, name) => empty
+      case Value.Variable(_, name)                      => empty
     }
   }
-    def patternLoop(pattern : Pattern[UType]) : ReferenceSet = {
-      val empty = ReferenceSet.empty;
-      def fold(stuff: Chunk[Pattern[UType]]): ReferenceSet = stuff.foldLeft(empty) { case (acc, next) => acc ++ patternLoop(next) }
-      pattern match {
-        case _: WildcardPattern[_] => empty
-        case AsPattern(_, innerPattern, name) =>patternLoop(innerPattern)
-        case _: UnitPattern[_] => empty
-        case LiteralPattern(_, _)=> empty
-        case _: EmptyListPattern[_] => empty
-        case HeadTailPattern(_, headPattern, tailPattern) => patternLoop(headPattern) ++ patternLoop(tailPattern)
-        case TuplePattern(_, patterns) => fold(patterns)
-        case ConstructorPattern(_, patternName, patterns) =>  empty.withConstructor(patternName) ++fold(patterns)
-      }
+  def patternLoop(pattern: Pattern[UType]): ReferenceSet = {
+    val empty = ReferenceSet.empty;
+    def fold(stuff: Chunk[Pattern[UType]]): ReferenceSet = stuff.foldLeft(empty) { case (acc, next) =>
+      acc ++ patternLoop(next)
     }
+    pattern match {
+      case _: WildcardPattern[_]                        => empty
+      case AsPattern(_, innerPattern, name)             => patternLoop(innerPattern)
+      case _: UnitPattern[_]                            => empty
+      case LiteralPattern(_, _)                         => empty
+      case _: EmptyListPattern[_]                       => empty
+      case HeadTailPattern(_, headPattern, tailPattern) => patternLoop(headPattern) ++ patternLoop(tailPattern)
+      case TuplePattern(_, patterns)                    => fold(patterns)
+      case ConstructorPattern(_, patternName, patterns) => empty.withConstructor(patternName) ++ fold(patterns)
+    }
+  }
 }
--- morphir/runtime/test/src/org/finos/morphir/runtime/quick/GatherRefsSpec.scala
+++ morphir/runtime/test/src/org/finos/morphir/runtime/quick/GatherRefsSpec.scala
@@ -20,72 +20,59 @@ import zio.{test as _, *}
 import scala.collection.immutable.ListMap
 
 object GatherRefsSpec extends MorphirBaseSpec {
-  val dist = EvaluationLibrary.loadDistribution("./examples/morphir-elm-projects/evaluator-tests/morphir-ir.json")
+  val dist     = EvaluationLibrary.loadDistribution("./examples/morphir-elm-projects/evaluator-tests/morphir-ir.json")
   val mattDist = EvaluationLibrary.loadDistribution("./examples/morphir-elm-projects/matt-instrument/morphir-ir.json")
   val maestroDist = EvaluationLibrary.loadDistribution("./examples/morphir-elm-projects/maestro-sdk/morphir-ir.json")
   val mappingDist = EvaluationLibrary.loadDistribution("./examples/morphir-elm-projects/mapping-logic/morphir-ir.json")
-  val example : FQName = FQName.fromString("Morphir.SDK:Basics:and")
-  val existing = Native.native.keys
-//
-//  class ListThing(name: String) {
-//    val ref          = FQName.fromString(name)
-//    val (pkg, mod, loc) = (ref.getPackagePath, ref.getModulePath, ref.localName)
-//    val value           = lib.lookupValueSpecification(PackageName(pkg), ModuleName(mod), loc).get
-//    val listArg         = value.inputs(0)._2
-//  }
-//  val withParam   = new ListThing("Morphir.Examples.App:TypeCheckerTests:withParam")
-//  val withInt     = new ListThing("Morphir.Examples.App:TypeCheckerTests:withInt")
-//  val listConcept = Concept.List(Concept.Integer)
-//  val toType      = ToMorphirType.summon[Concept]
-//  val intListType = toType.withAttributesOf(listConcept).morphirType
-
+  val example: FQName = FQName.fromString("Morphir.SDK:Basics:and")
+  val existing        = Native.native.keys
 
   def spec = suite("Exploration")(
     suite("From Distribution")(
       test("Everything") {
         val stuff = GatherReferences.fromDistributions(dist)
-        val defs = stuff.definitions.map(_.toString).mkString("\n")
+        val defs  = stuff.definitions.map(_.toString).mkString("\n")
         assertTrue(defs == "")
       },
       test("SDK Defs") {
-        val stuff = GatherReferences.fromDistributions(dist)
-        val defs = stuff.definitions.filter(_.getPackagePath == example.getPackagePath)
+        val stuff  = GatherReferences.fromDistributions(dist)
+        val defs   = stuff.definitions.filter(_.getPackagePath == example.getPackagePath)
         val string = defs.map(_.toString).mkString("\n")
         assertTrue(string == "")
       },
-      test("SDK Defs from Mapping"){
-        val stuff = GatherReferences.fromDistributions(mattDist)
-        val defs = stuff.definitions.filter(_.getPackagePath == example.getPackagePath)
+      test("SDK Defs from Mapping") {
+        val stuff  = GatherReferences.fromDistributions(mattDist)
+        val defs   = stuff.definitions.filter(_.getPackagePath == example.getPackagePath)
         val string = defs.map(_.toString).mkString("\n")
         assertTrue(string == "")
       },
       test("SDK Defs from Everywhere") {
-        val stuff = GatherReferences.fromDistributions(mattDist, mappingDist, maestroDist)
-        val defs = stuff.definitions.filter(_.getPackagePath == example.getPackagePath)
+        val stuff  = GatherReferences.fromDistributions(mattDist, mappingDist, maestroDist)
+        val defs   = stuff.definitions.filter(_.getPackagePath == example.getPackagePath)
         val string = defs.map(_.toString).mkString("\n")
         assertTrue(string == "")
       },
       test("Missing Defs from Mapping") {
-        val stuff = GatherReferences.fromDistributions(mattDist)
-        val defs = stuff.definitions.filter(_.getPackagePath == example.getPackagePath).diff(existing.toSet)
+        val stuff  = GatherReferences.fromDistributions(mattDist)
+        val defs   = stuff.definitions.filter(_.getPackagePath == example.getPackagePath).diff(existing.toSet)
         val string = defs.map(_.toString).mkString("\n")
         assertTrue(string == "")
       },
       test("Missing SDK Defs from Everywhere") {
-        val stuff = GatherReferences.fromDistributions(mattDist, mappingDist, maestroDist)
-        val defs = stuff.definitions.filter(_.getPackagePath == example.getPackagePath).diff(existing.toSet)
+        val stuff  = GatherReferences.fromDistributions(mattDist, mappingDist, maestroDist)
+        val defs   = stuff.definitions.filter(_.getPackagePath == example.getPackagePath).diff(existing.toSet)
         val string = defs.map(_.toString).mkString("\n")
         assertTrue(string == "")
       },
-      test("Only actually used"){
-        val stuff = GatherReferences.fromDistributions(mattDist)
+      test("Only actually used") {
+        val stuff     = GatherReferences.fromDistributions(mattDist)
         val moreStuff = GatherReferences.fromEntrySet(stuff, mattDist, mappingDist, maestroDist)
-        val defs = moreStuff.definitions//.filter(_.getPackagePath == example.getPackagePath)//.diff(existing.toSet)
+        val defs   = moreStuff.definitions // .filter(_.getPackagePath == example.getPackagePath)//.diff(existing.toSet)
         val string = defs.map(_.toString).mkString("\n")
         assertTrue(string == "")
 
       }
-    ),
-  )
+    )
+  ) @@ ignore @@ TestAspect.tag("Exploration code, not ready to be actual test")
 
 }
