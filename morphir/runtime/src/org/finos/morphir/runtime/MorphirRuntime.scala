7d76588358c2ef40e12222084d9eb3a297a4f605
Edward Peters <edwardstuyvesantpeters@gmail.com>
2023-08-24 17:26:37 -0700

    Incremental

--- morphir/runtime/src/org/finos/morphir/runtime/MorphirRuntime.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/MorphirRuntime.scala
@@ -10,7 +10,7 @@ import org.finos.morphir.runtime.environment.MorphirEnv
 import org.finos.morphir.runtime.exports.RTAction
 import org.finos.morphir.runtime.quick.QuickMorphirRuntime
 trait MorphirRuntime[TA, VA] {
-  def evaluate(entryPoint: Value[TA, VA], params: Value[TA, VA]*): RTAction[MorphirEnv, MorphirRuntimeError, Data]
+  def evaluate(entryPoint: Value[TA, VA], param: Value[TA, VA], params: Value[TA, VA]*): RTAction[MorphirEnv, MorphirRuntimeError, Data]
   def evaluate(entryPoint: Value[TA, VA], params: Data*): RTAction[MorphirEnv, MorphirRuntimeError, Data]
   def evaluate(entryPoint: FQName, params: Data): RTAction[MorphirEnv, MorphirRuntimeError, Data]
   def evaluate(entryPoint: FQName, params: Value[TA, VA]): RTAction[MorphirEnv, MorphirRuntimeError, Data]
