5e496e799131df10483879c57925cdae15d15887
Edward Peters <edwardstuyvesantpeters@gmail.com>
2023-08-25 21:26:16 -0700

    Incremental

--- morphir/src/org/finos/morphir/runtime/RTExecutionContext.scala
+++ morphir/src/org/finos/morphir/runtime/RTExecutionContext.scala
@@ -9,14 +9,16 @@ object EnableTyper {
 
 final case class RTExecutionContext(options: RTExecutionContext.Options)
 object RTExecutionContext {
-  val default: RTExecutionContext     = RTExecutionContext(RTExecutionContext.Options.default)
-  val typeChecked: RTExecutionContext = default.copy(options = Options.typeChecked)
+  val default: RTExecutionContext        = RTExecutionContext(RTExecutionContext.Options.default)
+  val typeChecked: RTExecutionContext    = default.copy(options = Options.typeChecked)
+  val notTypeChecked: RTExecutionContext = default.copy(options = Options.notTypeChecked)
 
   case class Options(
       enableTyper: EnableTyper
   )
   object Options {
-    val default: Options     = Options(EnableTyper.Enabled)
-    val typeChecked: Options = default.copy(enableType = true)
+    val default: Options        = Options(EnableTyper.Enabled)
+    val typeChecked: Options    = default.copy(enableType = true)
+    val notTypeChecked: Options = default.copy(enableType = false)
   }
 }
