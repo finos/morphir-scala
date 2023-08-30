879d58987bf4a1404bf8cbf8a1457b1323e2b8cc
Brian <Bouzomgi@gmail.com>
2023-08-30 16:11:25 -0400

    left test

--- examples/morphir-elm-projects/evaluator-tests/src/Morphir/Examples/App/StringTests.elm
+++ examples/morphir-elm-projects/evaluator-tests/src/Morphir/Examples/App/StringTests.elm
@@ -1,5 +1,12 @@
 module Morphir.Examples.App.StringTests exposing (..)
 import String exposing (append)
+import Morphir.SDK.String exposing (..)
 
 stringAppend : String -> String -> String
 stringAppend l r = append l r
+
+--Test: String/left
+stringLeftTest : () -> String
+stringLeftTest _ =
+    left 2 "Mulder"
+--expected = "Mu"
