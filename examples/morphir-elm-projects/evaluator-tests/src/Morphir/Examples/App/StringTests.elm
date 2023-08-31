b393bb3acdd2d5a2e539a24fced596f9c38df47b
Brian <Bouzomgi@gmail.com>
2023-08-31 18:17:41 -0400

    String tests 2

--- examples/morphir-elm-projects/evaluator-tests/src/Morphir/Examples/App/StringTests.elm
+++ examples/morphir-elm-projects/evaluator-tests/src/Morphir/Examples/App/StringTests.elm
@@ -15,3 +15,15 @@ stringRightTest : () -> String
 stringRightTest _ =
     right 2 "Scully"
 --expected = "ly"
+
+--Test: String/fromInt
+stringFromIntTest : () -> String
+stringFromIntTest _ =
+    fromInt 25
+--expected = "25"
+
+--Test: String/fromFloat
+stringFromFloatTest : () -> String
+stringFromFloatTest _ =
+    fromFloat 1.5
+--expected = "1.5"
