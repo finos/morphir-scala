97fbf420536d81267e92d1fb3a8f6c2b3038e431
Brian <Bouzomgi@gmail.com>
2023-08-31 18:17:10 -0400

    string tests

--- examples/morphir-elm-projects/evaluator-tests/src/Morphir/Examples/App/StringTests.elm
+++ examples/morphir-elm-projects/evaluator-tests/src/Morphir/Examples/App/StringTests.elm
@@ -3,3 +3,15 @@ import Morphir.SDK.String as String
 
 stringAppend : String -> String -> String
 stringAppend l r = append l r
+
+--Test: String/left
+stringLeftTest : () -> String
+stringLeftTest _ =
+    left 2 "Mulder"
+--expected = "Mu"
+
+--Test: String/right
+stringRightTest : () -> String
+stringRightTest _ =
+    right 2 "Scully"
+--expected = "ly"
