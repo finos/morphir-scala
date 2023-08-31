d3b9e6edc6d645eb9a7a118bc5e33c776cc29545
DmitryKozinets <dkozinets@yahoo.com>
2023-08-31 09:30:48 -0400

    dict tests

--- examples/morphir-elm-projects/evaluator-tests/src/Morphir/Examples/App/DictionaryTests.elm
+++ examples/morphir-elm-projects/evaluator-tests/src/Morphir/Examples/App/DictionaryTests.elm
@@ -5,24 +5,29 @@ import Dict exposing (Dict)
 {-
 -}
 
+--Test: Dict/toList
 dictToListTest: () -> List (Int, String)
 dictToListTest _ =
   let
     dict = Dict.fromList [(1, "Red"), (2, "Blue"), (3, "Orange")]
   in
     Dict.toList dict
+--expected = [(1, "Red"), (2, "Blue"), (3, "Orange")]
 
+--Test: Dict/filter
 dictFilterTest: () -> Dict Int String
 dictFilterTest _ =
     let
         dict = Dict.fromList [(1, "Red"), (2, "Blue"), (3, "Blue"), (4, "Blue"), (5, "Green")]
     in
         Dict.filter (\k v -> k > 2 && v == "Blue") dict
+--expected = [(3, "Blue"), (4, "Blue")]
 
 --Test: Dict/fromList
 dictFromListTest : () -> Dict Int String
 dictFromListTest _ =
     Dict.fromList [(1, "Red"), (2, "Blue"), (3, "Orange"), (4, "White"), (5, "Green")]
+--expected = Dict.fromList [(1, "Red"), (2, "Blue"), (3, "Orange"), (4, "White"), (5, "Green")]
 
 --Test: Dict/Get
 dictGetTest : () -> Maybe String
@@ -32,3 +37,15 @@ dictGetTest _ =
     in
         Dict.get "Tom" animals
 --expected = Just "Cat"
+
+--Test: Dict/empty
+dictEmptyTest : () -> Dict String Int
+dictEmptyTest _ =
+        Dict.empty
+--expected = Dict.empty
+
+--Test: Dict/singleton
+dictSingletonTest : () -> Dict Int String
+dictSingletonTest _ =
+    Dict.singleton 6 "Puppies"
+--expected = Dict(6 -> "Puppies")
