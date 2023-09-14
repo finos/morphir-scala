d7ad75e14d3ad5b8ee3ed6106aa7c81c52e53809
Alexander Ioffe <deusaquilus@gmail.com>
2023-09-14 00:39:07 -0400

    New Dynamic Native API, Unwrappers => Coercers
    Result => RTValue
    
    Remove redundant 'Result' from Result.Map/List/Function
    
    Rename Result.SetResult => Result.Set, move coersions to separate area
    
    Streamline numeric functions

---
+++ morphir/runtime/src/org/finos/morphir/runtime/quick/NativeFunctionAdapter.scala
@@ -0,0 +1,35 @@
+package org.finos.morphir.runtime.quick
+
+import org.finos.morphir.{Hints, MorphirTag, naming}
+import org.finos.morphir.naming.*
+import org.finos.morphir.runtime.quick.RTValue.Primitive
+
+sealed trait NativeFunctionAdapter {
+  def dnf: DynamicNativeFunction
+  def realize: SDKValue.SDKNativeInnerFunction
+}
+
+// format: off
+object NativeFunctionAdapter {
+  case class Fun1[T1 <: RTValue, R <: RTValue](val dnf: DynamicNativeFunction1[T1, R])(implicit u1: Coercer[T1]) extends NativeFunctionAdapter {
+    def realize: SDKValue.SDKNativeInnerFunction =
+      SDKValue.SDKNativeInnerFunction { new NativeFunctionSignatureAdv.Fun1(loop => r1 => dnf.f(NativeContext(loop))(u1.coerce(r1))) }
+  }
+  case class Fun2[T1 <: RTValue, T2 <: RTValue, R <: RTValue](val dnf: DynamicNativeFunction2[T1, T2, R])(implicit u1: Coercer[T1], u2: Coercer[T2]) extends NativeFunctionAdapter {
+    def realize: SDKValue.SDKNativeInnerFunction =
+      SDKValue.SDKNativeInnerFunction { new NativeFunctionSignatureAdv.Fun2(loop => (r1, r2) => dnf.f(NativeContext(loop))(u1.coerce(r1), u2.coerce(r2))) }
+  }
+  case class Fun3[T1 <: RTValue, T2 <: RTValue, T3 <: RTValue, R <: RTValue](val dnf: DynamicNativeFunction3[T1, T2, T3, R])(implicit u1: Coercer[T1], u2: Coercer[T2], u3: Coercer[T3]) extends NativeFunctionAdapter {
+    def realize: SDKValue.SDKNativeInnerFunction =
+      SDKValue.SDKNativeInnerFunction { new NativeFunctionSignatureAdv.Fun3(loop => (r1, r2, r3) => dnf.f(NativeContext(loop))(u1.coerce(r1), u2.coerce(r2), u3.coerce(r3))) }
+  }
+  case class Fun4[T1 <: RTValue, T2 <: RTValue, T3 <: RTValue, T4 <: RTValue, R <: RTValue](val dnf: DynamicNativeFunction4[T1, T2, T3, T4, R])(implicit u1: Coercer[T1], u2: Coercer[T2], u3: Coercer[T3], u4: Coercer[T4]) extends NativeFunctionAdapter {
+    def realize: SDKValue.SDKNativeInnerFunction =
+      SDKValue.SDKNativeInnerFunction { new NativeFunctionSignatureAdv.Fun4(loop => (r1, r2, r3, r4) => dnf.f(NativeContext(loop))(u1.coerce(r1), u2.coerce(r2), u3.coerce(r3), u4.coerce(r4))) }
+  }
+  case class Fun5[T1 <: RTValue, T2 <: RTValue, T3 <: RTValue, T4 <: RTValue, T5 <: RTValue, R <: RTValue](val dnf: DynamicNativeFunction5[T1, T2, T3, T4, T5, R])(implicit u1: Coercer[T1], u2: Coercer[T2], u3: Coercer[T3], u4: Coercer[T4], u5: Coercer[T5]) extends NativeFunctionAdapter {
+    def realize: SDKValue.SDKNativeInnerFunction =
+      SDKValue.SDKNativeInnerFunction { new NativeFunctionSignatureAdv.Fun5(loop => (r1, r2, r3, r4, r5) => dnf.f(NativeContext(loop))(u1.coerce(r1), u2.coerce(r2), u3.coerce(r3), u4.coerce(r4), u5.coerce(r5))) }
+  }
+}
+// format: on
