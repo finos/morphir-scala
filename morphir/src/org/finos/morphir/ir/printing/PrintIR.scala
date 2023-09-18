c7b2c4bf42236b7905cc15c9268694d26a2726e9
edwardpeters <edwardstuyvesantpeters@gmail.com>
2023-09-18 11:49:09 -0700

    Print ir depth limit (#418)
    * Branch for limiting PrintIR depth
    
    * Several PrintIR updates
    
    * Additional tests for constructor cases
    
    * Incremental
    
    * Formatting
    
    * Incremental
    
    * Reformatting
    
    * Reformatting
    
    * Reformatting
    
    * Reformatting
    
    * Reformatting
    
    * Incremental
    
    * Reformatting
    
    * Incremental
    
    * Reformatting
    
    * Reformatting
    
    * Incremental
    
    * Formatting
    
    * Formatting
    
    * Formatting
    
    * Formatting
    
    * Comment to retrigger compilation

--- morphir/src/org/finos/morphir/ir/printing/PrintIR.scala
+++ morphir/src/org/finos/morphir/ir/printing/PrintIR.scala
@@ -2,55 +2,83 @@ package org.finos.morphir.ir.printing
 
 import fansi.Str
 import pprint.{Renderer, Tree, Truncated}
+
 import java.util.function.UnaryOperator
-import org.finos.morphir.naming._
-import org.finos.morphir.ir.Value.{Value => V}
+import org.finos.morphir.naming.*
+import org.finos.morphir.ir.Value.{emptyTuple, Value as V}
 import org.finos.morphir.ir.Value
-import org.finos.morphir.ir.Type.{Type => T}
+import org.finos.morphir.ir.Type.Type as T
 import org.finos.morphir.ir.Type
-import pprint.Tree.Literal
+import pprint.Tree.{Apply, Literal}
+//import org.finos.morphir.runtime.quick.Result //??
 import zio.Chunk
 
 object PrintIR {
   def apply(
       any: Any,
       detailLevel: DetailLevel = DetailLevel.BirdsEye,
-      fieldNames: FieldNames = FieldNames.Hide,
       defaultWidth: Int = 150
-  ) = new PrintIR(detailLevel, fieldNames, defaultWidth).apply(any)
+  ) = new PrintIR(detailLevel, defaultWidth).apply(any)
 }
 
-sealed trait DetailLevel {
-  def compressFQNames: Boolean
-  def compressReferences: Boolean
-}
+case class DetailLevel(
+    compressNames: Boolean,
+    compressFQNames: Boolean,
+    compressReferences: Boolean,
+    showFieldNames: Boolean,
+    fqnView: FQNameView,
+    depthLimit: Option[Int]
+)
 object DetailLevel {
-  object Detailed extends DetailLevel {
-    def compressFQNames    = false
-    def compressReferences = false
+  object Detailed extends DetailLevel(
+        compressNames = false,
+        compressFQNames = false,
+        compressReferences = false,
+        showFieldNames = true,
+        fqnView = FQNameView.Full,
+        depthLimit = None
+      )
+  object Medium extends DetailLevel(
+        compressNames = true,
+        compressFQNames = true,
+        compressReferences = false,
+        showFieldNames = true,
+        fqnView = FQNameView.ModuleLocal,
+        depthLimit = Some(3)
+      )
+  object BirdsEye extends DetailLevel(
+        compressNames = true,
+        compressFQNames = true,
+        compressReferences = true,
+        showFieldNames = false,
+        fqnView = FQNameView.LocalOnly,
+        depthLimit = Some(2)
+      )
+
+}
+sealed trait FQNameView {
+  def apply(fqn: FQName): String
+}
+object FQNameView {
+
+  object Full extends FQNameView {
+    def apply(fqn: FQName): String = fqn.toString
   }
-  object Medium extends DetailLevel {
-    def compressFQNames    = true
-    def compressReferences = false
+  object ModuleLocal extends FQNameView {
+    def apply(fqn: FQName): String = s"${fqn.modulePath}:${fqn.localName.toCamelCase}"
   }
-  object BirdsEye extends DetailLevel {
-    def compressFQNames    = true
-    def compressReferences = true
+
+  object LocalOnly extends FQNameView {
+    def apply(fqn: FQName): String = fqn.localName.toCamelCase
   }
 }
 
 sealed trait FieldNames
-object FieldNames {
-  object Show extends FieldNames
-  object Hide extends FieldNames
-}
 
-class PrintIR(
+case class PrintIR(
     detailLevel: DetailLevel,
-    fieldNames: FieldNames,
     val defaultWidth: Int
 ) extends pprint.Walker {
-  val showFieldNames                = fieldNames == FieldNames.Show
   val escapeUnicode                 = false
   val defaultHeight: Int            = Integer.MAX_VALUE
   val defaultIndent: Int            = 2
@@ -66,43 +94,98 @@ class PrintIR(
     fansi.Str.join(tokenized)
   }
 
-  def treeify(x: Any): Tree = this.treeify(x, escapeUnicode, showFieldNames)
+  def withDepth(depth: Int): PrintIR = this.copy(detailLevel = detailLevel.copy(depthLimit = Some(depth)))
 
-  object MorphirValue {
-    def unapply(any: Any) =
-      any match {
-        case _: Value.Value[_, _] =>
-          Some(
-            any
-              .getClass()
-              .getName()
-              .replace("org.finos.morphir.ir.internal.", "")
-              .replace("$", ".")
-          )
-        case _ =>
-          None
-      }
+  def treeify(x: Any): Tree = this.treeify(x, escapeUnicode, this.detailLevel.showFieldNames)
+
+  def fqnv(fqn: FQName): String = detailLevel.fqnView(fqn)
+  def simplifyName(full: String): String =
+    full
+      .replace("$", ".")
+      .replace("org.finos.morphir.ir.TypeModule.Type.", "")
+      .replace("org.finos.morphir.ir.internal.Value.", "")
+      .replace("org.finos.morphir.ir.internal.Pattern.", "")
+      .replace("org.finos.morphir.ir.Literal.Literal.", "")
+
+  /**
+   * Extractor for any MorphirIR we want to treat specially for naming/depth limiting. Returns a prefix used to clarify
+   * which nodes are which, for help with duplicate names.
+   */
+  object AstNode {
+    def unapply(any: Any): Option[String] = any match {
+      case _: Type.Type[_]                                     => Some("T.")
+      case _: Value.Value[_, _]                                => Some("V.")
+      case _: Value.Pattern[_]                                 => Some("Pattern.")
+      case _: Value.Specification[_]                           => Some("VSpec.")
+      case _: Value.Definition[_, _]                           => Some("VDef.")
+      case _: Type.Specification[_]                            => Some("TSpec.")
+      case _: Type.Definition[_]                               => Some("TDef.")
+      case other if other.getClass.getName.contains("RTValue") => Some("RT.")
+      case _                                                   => None
+    }
   }
 
-  override def treeify(x: Any, escapeUnicode: Boolean, showFieldNames: Boolean): Tree = x match {
-    case fqname: FQName if (detailLevel.compressFQNames) =>
-      Tree.Literal(fqname.toString)
+  override def treeify(x: Any, escapeUnicode: Boolean, showFieldNames: Boolean): Tree = {
+    val node_prefix: String = x match {
+      case AstNode(p) => p
+      case _          => ""
+    }
+    val existing: Tree = detailLevel.depthLimit match {
+      case Some(depth) => treeifyWithDepth(x, depth)
+      case None        => treeifyNoDepth(x)
+    }
+    existing match {
+      case Apply(prefix, body) => Apply(node_prefix + simplifyName(prefix), body)
+      case Literal(body)       => Literal(node_prefix + simplifyName(body))
+      case other               => other
+    }
 
-    case T.Reference(_, fqname, Chunk()) if (detailLevel.compressReferences) =>
-      Tree.Literal(s"${fqname.toString}")
+  }
 
-    case T.Reference(_, fqname, tpes) if (detailLevel.compressReferences) =>
-      Tree.Literal(s"${fqname.toString}[${tpes.map(_.toString).mkString(", ")}]")
-
-    case MorphirValue(name) =>
-      val existingTree =
-        super.treeify(x, escapeUnicode, showFieldNames) match {
-          case Tree.Apply(_, content) => content
-          case other                  => Iterator(other)
+  def treeifyWithDepth(x: Any, depth: Int): Tree =
+    x match {
+      case AstNode(_) if depth <= 0 =>
+        x match {
+          // Leafs can display even at depth 0
+          // Assume compression for these cases
+          case name: Name => Tree.Literal(name.toCamelCase)
+          case T.Reference(_, fqn, Chunk()) =>
+            Tree.Literal(s"Ref(${fqnv(fqn)})")
+          case T.Reference(_, fqn, _) =>
+            Tree.Literal(s"Ref(${fqnv(fqn)}) [..]")
+          case T.Variable(_, name) => Tree.Literal(s"Variable(${name.toCamelCase})")
+          case V.Reference(_, fqn) =>
+            Tree.Literal(s"Ref(${fqnv(fqn)})")
+          case V.Literal(_, l)     => treeifyHelper(l)
+          case V.Variable(_, name) => Tree.Literal(s"Variable(${name.toCamelCase})")
+          case other               => Tree.Literal(s"${other.getClass.getName}(..)")
         }
-      Tree.Apply(s"${name}", existingTree)
+      case AstNode(_) =>
+        x match {
+          case V.Record(_, fields) =>
+            if (fields.length <= depth)
+              withDepth(depth - fields.length).treeifyHelper(x)
+            else
+              Tree.Literal(x.getClass.getName)
+          case other => withDepth(depth - 1).treeifyHelper(other)
+        }
+      case other => treeifyHelper(other)
+    }
+  def treeifyNoDepth(x: Any): Tree =
+    treeifyHelper(x)
 
-    case other => super.treeify(other, escapeUnicode, showFieldNames)
+  def treeifyHelper(x: Any): Tree = x match {
+    case name: Name if (detailLevel.compressNames) => Tree.Literal(name.toCamelCase)
+    case fqn: FQName if (detailLevel.compressFQNames) =>
+      Tree.Literal(fqnv(fqn))
+
+    case T.Reference(_, fqn, Chunk()) if (detailLevel.compressReferences) =>
+      Tree.Literal(s"Ref(${fqnv(fqn)})")
+
+    case T.Reference(_, fqn, tpes) if (detailLevel.compressReferences) =>
+      Tree.Apply(s"Ref(${fqnv(fqn)}):", tpes.map(treeify(_)).iterator)
+
+    case other => super.treeify(other, escapeUnicode, this.detailLevel.showFieldNames)
   }
 
   def tokenize(x: Any): Iterator[fansi.Str] = {
