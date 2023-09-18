0c1274cde2333327815408f33a933e62e5925871
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-09-18 18:04:25 -0400

    Fix warnings in DeriverInstances.scala that are now errors upon upgrading to Scala 2.13.12

--- morphir/src/org/finos/morphir/datamodel/DeriverInstances.scala
+++ morphir/src/org/finos/morphir/datamodel/DeriverInstances.scala
@@ -13,72 +13,72 @@ import scala.collection.mutable.LinkedHashMap
 trait DeriverInstances {
 
   implicit val booleanDeriver: CustomDeriver[Boolean] = new CustomDeriver[Boolean] {
-    def derive(value: Boolean) = Data.Boolean(value)
-    def concept                = Concept.Boolean
+    def derive(value: Boolean):Data = Data.Boolean(value)
+    def concept:Concept                = Concept.Boolean
   }
 
   implicit val byteDeriver: CustomDeriver[Byte] = new CustomDeriver[Byte] {
-    def derive(value: Byte) = Data.Byte(value)
-    def concept             = Concept.Byte
+    def derive(value: Byte):Data = Data.Byte(value)
+    def concept:Concept             = Concept.Byte
   }
 
   implicit val doubleDeriver: CustomDeriver[Double] = new CustomDeriver[Double] {
-    def derive(value: Double) = Data.Float(value)
-    def concept               = Concept.Float
+    def derive(value: Double):Data = Data.Float(value)
+    def concept:Concept               = Concept.Float
   }
 
   implicit val floatDeriver: CustomDeriver[Float] = new CustomDeriver[Float] {
-    def derive(value: Float) = Data.Float(value.toDouble)
-    def concept              = Concept.Float
+    def derive(value: Float):Data = Data.Float(value.toDouble)
+    def concept: Concept              = Concept.Float
   }
 
   implicit val bigDecimalDeriver: CustomDeriver[BigDecimal] = new CustomDeriver[BigDecimal] {
-    def derive(value: BigDecimal) = Data.Decimal(value)
-    def concept                   = Concept.Decimal
+    def derive(value: BigDecimal):Data = Data.Decimal(value)
+    def concept:Concept                   = Concept.Decimal
   }
 
   implicit val bigIntDeriver: CustomDeriver[BigInt] = new CustomDeriver[BigInt] {
-    def derive(value: BigInt) = Data.Integer(value)
-    def concept               = Concept.Integer
+    def derive(value: BigInt):Data = Data.Integer(value)
+    def concept:Concept               = Concept.Integer
   }
 
   implicit val shortDeriver: CustomDeriver[Short] = new CustomDeriver[Short] {
-    def derive(value: Short) = Data.Int16(value)
-    def concept              = Concept.Int16
+    def derive(value: Short):Data = Data.Int16(value)
+    def concept:Cncept              = Concept.Int16
   }
 
   implicit val intDeriver: CustomDeriver[Int] = new CustomDeriver[Int] {
-    def derive(value: Int) = Data.Int32(value)
-    def concept            = Concept.Int32
+    def derive(value: Int):Data = Data.Int32(value)
+    def concept:Concept            = Concept.Int32
   }
 
   implicit val longDeriver: CustomDeriver[Long] = new CustomDeriver[Long] {
-    def derive(value: Long) = Data.Int64(value)
+    def derive(value: Long):Data = Data.Int64(value)
     def concept             = Concept.Int64
   }
 
   implicit val stringDeriver: CustomDeriver[String] = new CustomDeriver[String] {
-    def derive(value: String) = Data.String(value)
+    def derive(value: String):Data = Data.String(value)
     def concept               = Concept.String
   }
 
   implicit val localDateDeriver: CustomDeriver[LocalDate] = new CustomDeriver[LocalDate] {
-    def derive(value: LocalDate) = Data.LocalDate(value)
+    def derive(value: LocalDate):Data = Data.LocalDate(value)
     def concept                  = Concept.LocalDate
   }
 
   implicit val monthDeriver: CustomDeriver[Month] = new CustomDeriver[Month] {
-    def derive(value: Month) = Data.Month(value)
+    def derive(value: Month):Data = Data.Month(value)
     def concept              = Concept.Month
   }
 
   implicit val localTimeDeriver: CustomDeriver[LocalTime] = new CustomDeriver[LocalTime] {
-    def derive(value: LocalTime) = Data.LocalTime(value)
+    def derive(value: LocalTime):Data = Data.LocalTime(value)
     def concept                  = Concept.LocalTime
   }
 
   implicit val charDeriver: CustomDeriver[Char] = new CustomDeriver[Char] {
-    def derive(value: Char) = Data.Char(value)
+    def derive(value: Char):Data = Data.Char(value)
     def concept             = Concept.Char
   }
 
