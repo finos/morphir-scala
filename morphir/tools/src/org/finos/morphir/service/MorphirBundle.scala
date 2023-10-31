2dad8c9e6cb5fc9a36d7feca8053fcd6ae3ad834
DmitryKozinets <dkozinets@yahoo.com>
2023-10-31 11:37:32 -0400

    MorphirBundle

--- morphir/tools/src/org/finos/morphir/service/MorphirBundle.scala
+++ morphir/tools/src/org/finos/morphir/service/MorphirBundle.scala
@@ -1,23 +1,13 @@
 package org.finos.morphir.service
 
-import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Paths}
-import org.finos.morphir.ir.distribution.Distribution
-import org.finos.morphir.ir.json.MorphirJsonSupport.*
-import org.finos.morphir.ir.MorphirIRFile
-import org.finos.morphir.ir.MorphirIRVersion
 import org.finos.morphir.util.vfile.*
-import scala.io.Source
 import zio.*
-import zio.json.*
 
 trait MorphirBundle {
   def bundle(outputBundleIRFilePath: VPath, irFiles: List[VPath]): Task[Unit]
 }
 
-object MorphirBundle {
-
-
+object MorphirBundle extends MorphirBundlePlatformSpecific {
   def bundle(outputBundleIRFilePath: VPath, irFiles: List[VPath]): ZIO[MorphirBundle, Throwable, Unit] =
     ZIO.serviceWithZIO[MorphirBundle](_.bundle(outputBundleIRFilePath, irFiles))
 }
