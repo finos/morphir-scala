34109ce505df563f181a47814f917c1e784e2f34
DmitryKozinets <dkozinets@yahoo.com>
2023-10-31 09:27:51 -0400

    refactor bundle

--- morphir/tools/src/org/finos/morphir/service/MorphirBundle.scala
+++ morphir/tools/src/org/finos/morphir/service/MorphirBundle.scala
@@ -7,7 +7,6 @@ import org.finos.morphir.ir.json.MorphirJsonSupport.*
 import org.finos.morphir.ir.MorphirIRFile
 import org.finos.morphir.ir.MorphirIRVersion
 import org.finos.morphir.util.vfile.*
-import org.finos.morphir.runtime.MorphirRuntimeError.*
 import scala.io.Source
 import zio.*
 import zio.json.*
@@ -27,8 +26,7 @@ object MorphirBundle {
         _             <- Console.printLine(s"\tirFiles: $irFiles")
         distributions <- ZIO.collectAll { irFiles.map { irFile => loadDistributionFromFileZIO(irFile.toString) } }
         bundle        <- ZIO.attempt { Distribution.toBundle(distributions: _*) }
-        morphirIRFile <- ZIO.attempt { MorphirIRFile(MorphirIRVersion.Default, bundle) }
-        writtenPath   <- writeDistrubtionToFileZIO(morphirIRFile, outputBundleIRFilePath)
+        writtenPath   <- writeDistrubtionToFileZIO(bundle, outputBundleIRFilePath)
         _             <- Console.printLine(s"\tBundle IR file created: $writtenPath")
         _             <- Console.printLine("Bundle command executed")
       } yield ()
@@ -38,17 +36,16 @@ object MorphirBundle {
       for {
         fileContents <- ZIO.readFile(fileName)
         morphirIRFile <- ZIO.fromEither(fileContents.fromJson[MorphirIRFile])
-          .mapError(s => throw new Exception(s"Parsing Error: $s"))
+          .mapError(error => throw new Exception(s"Parsing Error: $error"))
       } yield morphirIRFile.distribution
 
     // TODO: Possibly refactor when FileIO operations are completed
-    def writeDistrubtionToFileZIO(file: MorphirIRFile, path: VPath): Task[VPath] = {
-      import org.finos.morphir.ir.json.MorphirJsonSupport.*
+    def writeDistrubtionToFileZIO(bundle: Distribution, path: VPath): Task[VPath] =
       for {
-        newFileJson <- ZIO.attempt { file.toJson }
-        path        <- ZIO.attempt { Files.write(path.path.toNioPath, newFileJson.getBytes(StandardCharsets.UTF_8)) }
+        morphirIRFile <- ZIO.attempt { MorphirIRFile(MorphirIRVersion.Default, bundle) }
+        irJson        <- ZIO.attempt { morphirIRFile.toJson }
+        path          <- ZIO.attempt { Files.write(path.path.toNioPath, irJson.getBytes(StandardCharsets.UTF_8)) }
       } yield VPath(path)
-    }
   }
 
   def bundle(outputBundleIRFilePath: VPath, irFiles: List[VPath]): ZIO[MorphirBundle, Throwable, Unit] =
