934bdbe962e3de112a2cb6b5135f474ceb56ff2f
DmitryKozinets <dkozinets@yahoo.com>
2023-10-31 11:59:01 -0400

    MorphirBundlePlatformSpecific

--- morphir/tools/js/src/org/finos/morphir/service/MorphirBundlePlatformSpecific.scala
+++ morphir/tools/js/src/org/finos/morphir/service/MorphirBundlePlatformSpecific.scala
@@ -1,4 +1,9 @@
 package org.finos.morphir.service
 
-trait MorphirBundlePlatformSpecific {
+import org.finos.morphir.util.vfile.*
+import zio.json.*
 
+trait MorphirBundlePlatformSpecific {
+  val live: ULayer[MorphirBundle] = ZLayer.succeed(MorphirBundleLive)
+
+}
