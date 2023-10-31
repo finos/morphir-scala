95920845cb332a9afe4f80f9388cc68ec5ae9c08
DmitryKozinets <dkozinets@yahoo.com>
2023-10-31 12:01:20 -0400

    MorphirBundlePlatformSpecific

--- morphir/tools/js/src/org/finos/morphir/service/MorphirBundlePlatformSpecific.scala
+++ morphir/tools/js/src/org/finos/morphir/service/MorphirBundlePlatformSpecific.scala
@@ -6,4 +6,13 @@ import zio.json.*
 trait MorphirBundlePlatformSpecific {
   val live: ULayer[MorphirBundle] = ZLayer.succeed(MorphirBundleLive)
 
-}
+  object MorphirBundleLive extends MorphirBundle {
+    def bundle(outputBundleIRFilePath: VPath, irFiles: List[VPath]): Task[Unit] =
+      for {
+        _             <- Console.printLine("Bundle command executing")
+        _             <- Console.printLine(s"\toutputBundleIRFilePath: $outputBundleIRFilePath")
+        _             <- Console.printLine(s"\tirFiles: $irFiles")
+        _             <- Console.printLine(s"\tBundle IR file created: $writtenPath")
+        _             <- Console.printLine("Bundle command executed")
+      } yield ()
+  }
