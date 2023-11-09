aa6c6a4965720244b3f55440eab445e7ff676ba0
DmitryKozinets <dkozinets@yahoo.com>
2023-11-09 17:07:19 -0500

    more fixes

--- morphir/tools/js/src/org/finos/morphir/service/MorphirBundlePlatformSpecific.scala
+++ morphir/tools/js/src/org/finos/morphir/service/MorphirBundlePlatformSpecific.scala
@@ -9,7 +9,12 @@ trait MorphirBundlePlatformSpecific {
 
   object MorphirBundleLive extends MorphirBundle {
     def bundle(outputPath: VPath, irFiles: List[VPath]): Task[Unit] =
-      bundle(outputPath.path.toNioPath, irFiles.map(_.path.toNioPath))
+      for {
+        _ <- Console.printLine("Bundle command executing")
+        _ <- Console.printLine(s"\toutputPath: $outputPath")
+        _ <- Console.printLine(s"\tirFiles: $irFiles")
+        _ <- Console.printLine("Bundle command executed")
+      } yield ()
 
     def bundle(outputPath: VPath, irFiles: List[VPath]): Task[Unit] =
       for {
@@ -20,7 +25,12 @@ trait MorphirBundlePlatformSpecific {
       } yield ()
 
     def library(outputDir: VPath, irFiles: List[VPath]): Task[Unit] =
-      library(outputDir.path.toNioPath, irFiles.map(_.path.toNioPath))
+      for {
+        _ <- Console.printLine("Library command executing")
+        _ <- Console.printLine(s"\toutputDir: $outputDir")
+        _ <- Console.printLine(s"\tirFiles: $irFiles")
+        _ <- Console.printLine("Library command executed")
+      } yield ()
 
     def library(outputDir: Path, irFiles: List[Path]): Task[Unit] =
       for {
