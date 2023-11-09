a5d598bf9320183c7201611c416c07bb77351d4a
DmitryKozinets <dkozinets@yahoo.com>
2023-11-09 17:00:17 -0500

    more fixes

--- morphir/tools/js/src/org/finos/morphir/service/MorphirBundlePlatformSpecific.scala
+++ morphir/tools/js/src/org/finos/morphir/service/MorphirBundlePlatformSpecific.scala
@@ -1,5 +1,6 @@
 package org.finos.morphir.service
 
+import java.nio.file.Path
 import org.finos.morphir.util.vfile._
 import zio._
 
@@ -7,6 +8,9 @@ trait MorphirBundlePlatformSpecific {
   val live: ULayer[MorphirBundle] = ZLayer.succeed(MorphirBundleLive)
 
   object MorphirBundleLive extends MorphirBundle {
+    def bundle(outputPath: VPath, irFiles: List[VPath]): Task[Unit] =
+      bundle(outputPath.path.toNioPath, irFiles.map(_.path.toNioPath))
+
     def bundle(outputPath: VPath, irFiles: List[VPath]): Task[Unit] =
       for {
         _ <- Console.printLine("Bundle command executing")
@@ -16,6 +20,9 @@ trait MorphirBundlePlatformSpecific {
       } yield ()
 
     def library(outputDir: VPath, irFiles: List[VPath]): Task[Unit] =
+      library(outputDir.path.toNioPath, irFiles.map(_.path.toNioPath))
+
+    def library(outputDir: Path, irFiles: List[Path]): Task[Unit] =
       for {
         _ <- Console.printLine("Library command executing")
         _ <- Console.printLine(s"\toutputDir: $outputDir")
