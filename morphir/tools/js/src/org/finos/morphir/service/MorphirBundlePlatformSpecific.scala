23f46ece69277e8bd3bca333c9cb955cf18ba9b7
DmitryKozinets <dkozinets@yahoo.com>
2023-11-09 15:22:27 -0500

    more changes

--- morphir/tools/js/src/org/finos/morphir/service/MorphirBundlePlatformSpecific.scala
+++ morphir/tools/js/src/org/finos/morphir/service/MorphirBundlePlatformSpecific.scala
@@ -7,12 +7,20 @@ trait MorphirBundlePlatformSpecific {
   val live: ULayer[MorphirBundle] = ZLayer.succeed(MorphirBundleLive)
 
   object MorphirBundleLive extends MorphirBundle {
-    def bundle(outputBundleIRFilePath: VPath, irFiles: List[VPath]): Task[Unit] =
+    def bundle(outputPath: VPath, irFiles: List[VPath]): Task[Unit] =
       for {
         _ <- Console.printLine("Bundle command executing")
-        _ <- Console.printLine(s"\toutputBundleIRFilePath: $outputBundleIRFilePath")
+        _ <- Console.printLine(s"\toutputPath: $outputPath")
         _ <- Console.printLine(s"\tirFiles: $irFiles")
         _ <- Console.printLine("Bundle command executed")
+      } yield ()
+
+    def library(outputDir: VPath, irFiles: List[VPath]): Task[Unit] =
+      for {
+        _ <- Console.printLine("Library command executing")
+        _ <- Console.printLine(s"\toutputDir: $outputDir")
+        _ <- Console.printLine(s"\tirFiles: $irFiles")
+        _ <- Console.printLine("Library command executed")
       } yield ()
   }
 }
