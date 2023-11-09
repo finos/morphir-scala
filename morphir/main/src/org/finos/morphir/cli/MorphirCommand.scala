11945b8a1c0c49f82cd3e5320fe43f1e626abaff
DmitryKozinets <dkozinets@yahoo.com>
2023-11-09 15:17:39 -0500

    library command

--- morphir/main/src/org/finos/morphir/cli/MorphirCommand.scala
+++ morphir/main/src/org/finos/morphir/cli/MorphirCommand.scala
@@ -5,6 +5,7 @@ import java.nio.file.Path
 enum MorphirCommand {
   case Bundle(outputBundleIRFilePath: Path, irFiles: List[Path])
   case Develop(port: Int, host: String, projectDir: Path, openInBrowser: Boolean)
+  case Library(outputDir: Path, irFiles: List[Path])
   case Setup(morphirHomeDir: Path)
   case Test(irFiles: List[Path])
   case ElmDevelop(port: Int, host: String, projectDir: Path, openInBrowser: Boolean)
