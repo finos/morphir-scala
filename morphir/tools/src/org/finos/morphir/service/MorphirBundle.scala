ad7e30d0e742bbb4ec9ce465ae6901b3105e5a86
DmitryKozinets <dkozinets@yahoo.com>
2023-11-09 15:29:42 -0500

    more changes

--- morphir/tools/src/org/finos/morphir/service/MorphirBundle.scala
+++ morphir/tools/src/org/finos/morphir/service/MorphirBundle.scala
@@ -1,13 +1,20 @@
 package org.finos.morphir.service
 
+import java.nio.file.Path
 import org.finos.morphir.util.vfile.*
 import zio.*
 
 trait MorphirBundle {
-  def bundle(outputBundleIRFilePath: VPath, irFiles: List[VPath]): Task[Unit]
+  def bundle(outputPath: VPath, irFiles: List[VPath]): Task[Unit]
+  def bundle(outputPath: Path, irFiles: List[Path]): Task[Unit] =
+    bundle(VPath(outputPath), irFiles.map(VPath(_)))
+
 }
 
 object MorphirBundle extends MorphirBundlePlatformSpecific {
-  def bundle(outputBundleIRFilePath: VPath, irFiles: List[VPath]): ZIO[MorphirBundle, Throwable, Unit] =
-    ZIO.serviceWithZIO[MorphirBundle](_.bundle(outputBundleIRFilePath, irFiles))
+  def bundle(outputPath: VPath, irFiles: List[VPath]): ZIO[MorphirBundle, Throwable, Unit] =
+    ZIO.serviceWithZIO[MorphirBundle](_.bundle(outputPath, irFiles))
+
+  def bundle(outputPath: Path, irFiles: List[Path]): ZIO[MorphirBundle, Throwable, Unit] =
+    bundle(VPath(outputPath), irFiles.map(VPath(_)))
 }
