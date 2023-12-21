32161408ec9c93e3fa24b0064c703138e53da093
MichelleChan <6752211+michelchan@users.noreply.github.com>
2023-12-21 13:09:44 -0500

    String sdk

---
+++ morphir/src/org/finos/morphir/runtime/sdk/StringSDK.scala
@@ -0,0 +1,20 @@
+package org.finos.morphir.runtime.sdk
+
+import org.finos.morphir.datamodel.Schema.Primitive
+import org.finos.morphir.runtime.internal.{
+  DynamicNativeFunction,
+  DynamicNativeFunction1,
+  DynamicNativeFunction2,
+  DynamicNativeFunction3,
+  NativeContext
+}
+import org.finos.morphir.runtime.RTValue
+import org.finos.morphir.runtime.RTValue as RT
+import org.finos.morphir.runtime.RTValue.Primitive.String as RTString
+import org.finos.morphir.runtime.RTValue.{coerceList, coerceString}
+
+import scala.collection.StringOps
+
+object StringSDK {
+
+}
