0ad82f55ca780e7607af2e364eba0c894075aae5
MichelleChan <6752211+michelchan@users.noreply.github.com>
2023-12-28 11:20:17 -0500

    Decimal sdk

--- morphir/src/org/finos/morphir/runtime/sdk/DecimalSDK.scala
+++ morphir/src/org/finos/morphir/runtime/sdk/DecimalSDK.scala
@@ -7,4 +7,44 @@ object DecimalSDK {
       Decimal(result)
   }
 
+  val add = DynamicNativeFunction("add") {
+    (_: NativeContext) => () =>
+      Decimal(result)
+  }
+
+  val bps = DynamicNativeFunction("bps") {
+    (_: NativeContext) => () =>
+      Decimal(result)
+  }
+
+  val compare = DynamicNativeFunction("compare") {
+    (_: NativeContext) => () =>
+      Decimal(result)
+  }
+
+  val div = DynamicNativeFunction("div") {
+    (_: NativeContext) => () =>
+      Decimal(result)
+  }
+
+  val divWithDefault = DynamicNativeFunction("divWithDefault") {
+    (_: NativeContext) => () =>
+      Decimal(result)
+  }
+
+  val eq = DynamicNativeFunction("eq") {
+    (_: NativeContext) => () =>
+      Decimal(result)
+  }
+
+  val fromInt = DynamicNativeFunction("fromInt") {
+    (_: NativeContext) => () =>
+      Decimal(result)
+  }
+
+  val fromString = DynamicNativeFunction("fromString") {
+    (_: NativeContext) => () =>
+      Decimal(result)
+  }
+
 }
