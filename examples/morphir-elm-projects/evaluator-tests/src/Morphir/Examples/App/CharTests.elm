2528b28f7e1e02a309797ead8f9c321ba78660a3
MichelleChan <6752211+michelchan@users.noreply.github.com>
2024-02-22 16:51:21 -0500

    Adding char test

--- examples/morphir-elm-projects/evaluator-tests/src/Morphir/Examples/App/CharTests.elm
+++ examples/morphir-elm-projects/evaluator-tests/src/Morphir/Examples/App/CharTests.elm
@@ -19,6 +19,81 @@ charIsUpper ch =
     expected('A') = False
     expected('w') = True
 -}
-charIsLower : Char -> Bool
+charIsLower: Char -> Bool
 charIsLower ch =
     isLower ch
+
+
+{-|
+    Test: Char/isAlpha
+-}
+isAlpha: Char -> Bool
+isAlpha ch = isAlpha ch
+
+
+{-|
+    Test: Char/isAlphaNum
+-}
+isAlphaNum: Char -> Bool
+isAlphaNum ch = isAlphaNum ch
+
+
+{-|
+    Test: Char/isDigit
+-}
+isDigit: Char -> Bool
+isDigit ch = isDigit ch
+
+
+{-|
+    Test: Char/isOctDigit
+-}
+isOctDigit: Char -> Bool
+isOctDigit ch = isOctDigit ch
+
+{-|
+    Test: Char/isHexDigit
+-}
+isHexDigit: Char -> Bool
+isHexDigit ch = isHexDigit ch
+
+{-|
+    Test: Char/toUpper
+-}
+toUpper: Char -> Char
+toUpper ch = toUpper ch
+
+
+{-|
+    Test: Char/toLower
+-}
+toLower: Char -> Char
+toLower ch = toLower ch
+
+
+{-|
+    Test: Char/toLocaleUpper
+-}
+toLocaleUpper: Char -> Char
+toLocaleUpper ch = toLocaleUpper ch
+
+
+{-|
+    Test: Char/toLocaleLower
+-}
+toLocaleLower: Char -> Char
+toLocaleLower ch = toLocaleLower ch
+
+
+{-|
+    Test: Char/toCode
+-}
+toCode: Char -> Int
+toCode ch = toCode ch
+
+
+{-|
+    Test: Char/fromCode
+-}
+fromCode: Int -> Char
+fromCode int = fromCode int
