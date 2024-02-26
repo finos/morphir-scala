7659230075f4f527b4c7809b0a78f29a760c30eb
Edward Peters <11288157+edwardpeters@users.noreply.github.com>
2024-02-26 15:17:16 -0700

    incremental

--- morphir/runtime/src/org/finos/morphir/runtime/unit-testing/TestTree.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/unit-testing/TestTree.scala
@@ -6,26 +6,25 @@ sealed trait TestTree[T]
 type MorphirUnitTest = TestTree[RT]
 
 sealed trait SingleResult
-case class Passed() extends SingleResult
-case class Failed(msg : String) extends SingleResult
+case class Passed()            extends SingleResult
+case class Failed(msg: String) extends SingleResult
 
 object TestTree {
   case class Describe[T](desc: String, tests: List[TestTree[T]]) extends TestTree[T]
-  case class SingleTest[T](desc: String, expectThunk: T)
-  case class Concat[T](tests: List[TestTree[T]]) extends TestTree[T]
-  case class Todo[T](desc: String)               extends TestTree[T]
-  case class Skip[T](test: TestTree[T])          extends TestTree[T]
-  case class Only[T](test: TestTree[T])          extends TestTree[T]
+  case class SingleTest[T](desc: String, expectThunk: T)         extends TestTree[T]
+  case class Concat[T](tests: List[TestTree[T]])                 extends TestTree[T]
+  case class Todo[T](desc: String)                               extends TestTree[T]
+  case class Skip[T](test: TestTree[T])                          extends TestTree[T]
+  case class Only[T](test: TestTree[T])                          extends TestTree[T]
 
-  def containsOnly[T](tree : TestTree[T]) : Boolean = {
+  def containsOnly[T](tree: TestTree[T]): Boolean =
     tree match {
-      case Describe(_, tests) => tests.any(containsOnly)
-      case Concat(tests) => tests.any(containsOnly)
-      case Skip(inner) => containsOnly(inner)
-      case Only(_) => true
-      case _ => false
+      case Describe(_, tests) => tests.exists(containsOnly)
+      case Concat(tests)      => tests.exists(containsOnly)
+      case Skip(inner)        => containsOnly(inner)
+      case Only(_)            => true
+      case _                  => false
     }
-  }
 
   def fromRTValue(value: RT): MorphirUnitTest =
     value match {
@@ -39,6 +38,5 @@ object TestTree {
       case RT.ConstructorResult(FQString("Morphir.UnitTest:Expect:Skip"), List(test)) => Only(fromRTValue(test))
       case RT.ConstructorResult(FQString("Morphir.UnitTest:Expect:Only"), List(test)) => Only(fromRTValue(test))
     }
-
 
 }
