4e7bdeadd686de51f4a6e87d6cbdf9aaa213c3d1
Edward Peters <11288157+edwardpeters@users.noreply.github.com>
2024-02-27 09:38:40 -0700

    Incremental

--- morphir/runtime/src/org/finos/morphir/runtime/unit-testing/TestTree.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/unit-testing/TestTree.scala
@@ -39,7 +39,10 @@ object TestTree {
     tree match{
       case Describe(desc, tests) => "\t".repeat(depth) + desc + "\n" + tests.map(toReportHelper(_, depth + 1)).mkString("\n")
       case SingleTest(desc, Passed()) => "\t".repeat(depth) + desc : ": PASSED"
-      case Concat(tests) => tests.map(toReportHelper(_, depth)).mkString("\n")
+      case Concat(tests) => tests.map(toReportHelper(_, depth)).mkString("\n")   case Todo(excuse) => "\t".repeat(depth) + desc + ": TODO"
+      case Skip(desc, count) => "\t".repeat(depth) + desc + ": SKIPPED" +(if (count == 1) "" else s"($count tests skipped)")
+      case Error(desc, err) => "\t".repeat(depth) + desc + ": ERROR" : err.toString
+      case Only(inner) => toReportHelper(inner, depth)
    
 
     }
