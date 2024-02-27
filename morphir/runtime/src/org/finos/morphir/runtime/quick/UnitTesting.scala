f4613e482d1e01aaac5d1d4982012f46d633b972
Edward Peters <11288157+edwardpeters@users.noreply.github.com>
2024-02-27 11:50:07 -0700

    Incremental

--- morphir/runtime/src/org/finos/morphir/runtime/quick/UnitTesting.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/quick/UnitTesting.scala
@@ -144,7 +144,7 @@ object UnitTesting {
       import org.finos.morphir.ir.Value.Value.{List as ListValue, *}
       value match {
         case Apply(_, Apply(_, Reference(_, FQString("Morphir.UnitTest:Expect:equal")), arg1IR), arg2IR) =>
-          res = Some(
+          val res = Some(
             V.applyInferType(
               expectationType,
               V.reference(FQName.fromString("Morphir.UnitTest:Expect:equalIntrospected")),
