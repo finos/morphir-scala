02211e0a6e1d66a087eeae756e7af9397549bcc7
Edward Peters <11288157+edwardpeters@users.noreply.github.com>
2024-02-29 15:56:02 -0700

    Incremental

--- morphir/runtime/src/org/finos/morphir/runtime/SingleTestResult.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/SingleTestResult.scala
@@ -1,5 +1,7 @@
 package org.finos.morphir.runtime
 trait SingleTestResult {}
-case object Pass               extends SingleTestResult
-case class Failed(msg: String) extends SingleTestResult
-case class Err(e: Throwable)   extends SingleTestResult
+object SingleTestResult {
+  case object Pass               extends SingleTestResult
+  case class Failed(msg: String) extends SingleTestResult
+  case class Err(e: Throwable)   extends SingleTestResult
+}
