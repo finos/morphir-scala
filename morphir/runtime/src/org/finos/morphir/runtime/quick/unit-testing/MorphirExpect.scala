49e2232322e737a10f4ece8ba9802159855398ff
Edward Peters <11288157+edwardpeters@users.noreply.github.com>
2024-02-29 15:29:50 -0700

    Incremental

--- morphir/runtime/src/org/finos/morphir/runtime/quick/unit-testing/MorphirExpect.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/quick/unit-testing/MorphirExpect.scala
@@ -7,6 +7,7 @@ import org.finos.morphir.ir.{Type => T, Value => V}
 import org.finos.morphir.ir.Value.Pattern
 import org.finos.morphir.ir.Value.Value.{List as ListValue, Unit as UnitValue, *}
 import org.finos.morphir.runtime.SingleTestResult
+import org.finos.morphir.runtime.MorphirRuntimeError.*
 import org.finos.morphir.runtime.Extractors.Values.ApplyChain
 import org.finos.morphir.runtime.Extractors.{FQString, FQStringTitleCase}
 import org.finos.morphir.runtime.internal.{
