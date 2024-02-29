e0c285b3e7fe897772d9cdd532bff173ff3f16d9
Edward Peters <11288157+edwardpeters@users.noreply.github.com>
2024-02-29 15:13:23 -0700

    Incremental

--- morphir/runtime/src/org/finos/morphir/runtime/quick/unit-testing/MorphirExpect.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/quick/unit-testing/MorphirExpect.scala
@@ -47,16 +47,16 @@ sealed trait MorphirExpect {
   }
   def processThunk(args: List[TypedValue], context: CallStackFrame): SingleTestResult
 }
-sealed trait MorphirExpect1 {
+sealed trait MorphirExpect1[T, R] extends MorphirExpect {
   def arity = 1
-  def dynamicFunction: DynamicNativeFunction1
+  def dynamicFunction: DynamicNativeFunction1[T, R]
   def sdkFunction = NativeFunctionAdapter.Fun1(
     dynamicFunction
   ).realize
 }
-sealed trait MorphirExpect2 {
+sealed trait MorphirExpect2[T1, T2, R] extends MorphirExpect {
   def arity = 2
-  def dynamicFunction: DynamicNativeFunction2
+  def dynamicFunction: DynamicNativeFunction2[T1, T2, R]
   def sdkFunction = NativeFunctionAdapter.Fun2(
     dynamicFunction
   ).realize
