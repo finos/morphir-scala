81cd42038e9fd74ad8aa27f327fc54329f4d0a46
Edward Peters <11288157+edwardpeters@users.noreply.github.com>
2024-03-01 17:59:43 -0700

    Incremental

--- examples/morphir-elm-projects/unit-test-framework/example-project-tests/src/Example/FailingModuleTwo.elm
+++ examples/morphir-elm-projects/unit-test-framework/example-project-tests/src/Example/FailingModuleTwo.elm
@@ -1,1 +1,6 @@
 module ExampleTests.FailingModuleTwo exposing (..)
+import Morphir.SDK.Dict as Dict
+import Morphir.SDK.Set as Set
+import Morphir.UnitTest.Test exposing (..)
+import Morphir.UnitTest.Expect as Expect
+import Example.ExampleModule exposing (..)
