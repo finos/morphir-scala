6a7b9b6f260d5aa0f58d36f5aa70c1ad02431e3a
Edward Peters <11288157+edwardpeters@users.noreply.github.com>
2024-03-02 05:36:19 -0700

    Incremental

--- morphir/runtime/src/org/finos/morphir/runtime/TestSummary.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/TestSummary.scala
@@ -1,13 +1,16 @@
 package org.finos.morphir.runtime
+import org.finos.morphir.naming.*
 
 case class TestSummary(
     message: String,
-    success: Boolean // How should Incomplete be handled?
-
+    success: Boolean, // How should Incomplete be handled?
+    countsByModule: Map[(pkgName: PackageName, modName: ModuleName), TestResultCounts],
+    overallCounts: TestResultCounts
 )
 sealed trait OverallStatus
-
-case object Passed     extends OverallStatus
-case object Failed     extends OverallStatus
-case object Incomplete extends OverallStatus
-case class TestResultCounts(passed: Int, failed: Int, skipped: Int, todo: Int)
+object OverallStatus {
+  case object Passed     extends OverallStatus
+  case object Failed     extends OverallStatus
+  case object Incomplete extends OverallStatus
+}
+case class TestResultCounts(passed: Int, failed: Int, errors: Int, skipped: Int, todo: Int)
