623d2c103a39bb5487adfaf3df52d3b15e360ef6
Edward Peters <11288157+edwardpeters@users.noreply.github.com>
2024-03-01 18:10:52 -0700

    Incremental

--- morphir/runtime/src/org/finos/morphir/runtime/TestSummary.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/TestSummary.scala
@@ -3,4 +3,7 @@ package org.finos.morphir.runtime
 case class TestSummary(
     message: String,
     success: Boolean // How should Incomplete be handled?
+    
 )
+
+case class TestResultCounts(passed : Int, failed : Int, skipped : Int, todo : Int)
