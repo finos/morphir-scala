e8c0dc64250d192c1f3b26ec1d18d0ece6b33c55
Edward Peters <11288157+edwardpeters@users.noreply.github.com>
2024-03-02 23:58:01 -0700

    Incremental

--- examples/morphir-elm-projects/unit-test-framework/example-project-tests/src/Example/FailingModuleTwo.elm
+++ examples/morphir-elm-projects/unit-test-framework/example-project-tests/src/Example/FailingModuleTwo.elm
@@ -5,8 +5,16 @@ import Morphir.UnitTest.Test exposing (..)
 import Morphir.UnitTest.Expect as Expect
 import Example.ExampleModule exposing (..)
 
+--Infinite recursion generator, to test error conditions
+err : Int -> Int
+err x = err x
+
 failing : Test
-failing = only <| test "String Concat" <|
+failing = test "String Concat" <|
     \_ -> Expect.equal
         (String.concat ["One", "Two", "Three"])
         "One, Two, Three"
+
+errors : Test
+errors = test "Throws error"
+    \_ -> Expect.lessThan (err 0) (err 1)
