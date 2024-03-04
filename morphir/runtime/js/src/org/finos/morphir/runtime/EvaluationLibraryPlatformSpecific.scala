2aa02861dbb5523c7cff9e6d705393ebb1da6846
Edward Peters <11288157+edwardpeters@users.noreply.github.com>
2024-03-04 13:18:11 -0800

    Incremental

--- morphir/runtime/js/src/org/finos/morphir/runtime/EvaluationLibraryPlatformSpecific.scala
+++ morphir/runtime/js/src/org/finos/morphir/runtime/EvaluationLibraryPlatformSpecific.scala
@@ -1,4 +1,5 @@
 package org.finos.morphir.runtime
+import org.finos.morphir.ir.distribution.Distribution
 
 trait EvaluationLibraryPlatformSpecific {
   def loadDistribution(fileName: String): Distribution =
