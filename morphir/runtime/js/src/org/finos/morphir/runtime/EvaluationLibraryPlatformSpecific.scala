f552563444b3a3580fa16620a6ac486a72f3a3c3
Edward Peters <11288157+edwardpeters@users.noreply.github.com>
2024-03-06 12:21:43 -0800

    Incremental

--- morphir/runtime/js/src/org/finos/morphir/runtime/EvaluationLibraryPlatformSpecific.scala
+++ morphir/runtime/js/src/org/finos/morphir/runtime/EvaluationLibraryPlatformSpecific.scala
@@ -5,13 +5,6 @@ import scala.io.Source
 
 trait EvaluationLibraryPlatformSpecific {
   def loadDistribution(fileName: String): Distribution = {
-    val text = Source
-      .fromFile(fileName)
-      .getLines()
-      .mkString("\n")
-    val morphirIRFile = text.fromJson[MorphirIRFile]
-    morphirIRFile
-      .getOrElse(throw new Exception(s"Failed to load $fileName as distribution"))
-      .distribution
+    throw new Exception("Unsupported on this platform")
   }
 }
--- morphir/runtime/native/src/org/finos/morphir/runtime/EvaluationLibraryPlatformSpecific.scala
+++ morphir/runtime/native/src/org/finos/morphir/runtime/EvaluationLibraryPlatformSpecific.scala
@@ -4,14 +4,6 @@ import org.finos.morphir.ir.MorphirIRFile
 import scala.io.Source
 
 trait EvaluationLibraryPlatformSpecific {
-  def loadDistribution(fileName: String): Distribution = {
-    val text = Source
-      .fromFile(fileName)
-      .getLines()
-      .mkString("\n")
-    val morphirIRFile = text.fromJson[MorphirIRFile]
-    morphirIRFile
-      .getOrElse(throw new Exception(s"Failed to load $fileName as distribution"))
-      .distribution
-  }
+  def loadDistribution(fileName: String): Distribution =
+    throw new Exception("Unsupported on this platform")
 }
