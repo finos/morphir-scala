36bb033888b28a67b973870fcdcc4918794481e4
edwardpeters <edwardstuyvesantpeters@gmail.com>
2024-03-14 10:00:35 -0700

    Quick fix for interface error (#551)
    Co-authored-by: Edward Peters <11288157+edwardpeters@users.noreply.github.com>

--- morphir/runtime/src/org/finos/morphir/runtime/MorphirRuntime.scala
+++ morphir/runtime/src/org/finos/morphir/runtime/MorphirRuntime.scala
@@ -14,7 +14,7 @@ trait MorphirRuntime {
   type TypeAttribs
   type ValueAttribs
 
-  def runUnitTests(distributions: Distribution*): RTAction[MorphirEnv, MorphirRuntimeError, TestSummary]
+  def runUnitTests(): RTAction[MorphirEnv, MorphirRuntimeError, TestSummary]
 
   def evaluate(
       entryPoint: Value[TypeAttribs, ValueAttribs],
