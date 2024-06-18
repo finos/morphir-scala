49840062cfe86f55b749d07a8bdc018758b8e3ed
Alex Sieland <alexsieland@proton.me>
2024-06-18 11:29:12 -0400

    Test for million

--- examples/morphir-elm-projects/evaluator-tests/src/Morphir/Examples/App/DecimalTests.elm
+++ examples/morphir-elm-projects/evaluator-tests/src/Morphir/Examples/App/DecimalTests.elm
@@ -412,6 +412,16 @@ decimalHundred value =
         value
 
 
+{-| Test: Decimal/million
+value = 123
+expected = 12300000
+-}
+decimalMillion : Int -> Decimal
+decimalMillion value =
+    Decimal.million
+        value
+
+
 {-| Test: Decimal/hundredth
 value = 123
 expected = 1.23
