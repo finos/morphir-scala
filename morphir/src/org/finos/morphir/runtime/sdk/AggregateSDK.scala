87f7c20ef0df199be03414a21e315cfe628ad0c7
Alex Sieland <alexsieland@proton.me>
2024-07-18 10:30:37 -0400

    Add AggregateSDK object

--- morphir/src/org/finos/morphir/runtime/sdk/AggregateSDK.scala
+++ morphir/src/org/finos/morphir/runtime/sdk/AggregateSDK.scala
@@ -5,34 +5,25 @@ import org.finos.morphir.ir.Type
 import org.finos.morphir.runtime.*
 import org.finos.morphir.runtime.MorphirRuntimeError.{FailedCoercion, IllegalValue}
 import org.finos.morphir.runtime.internal.*
-import org.finos.morphir.runtime.RTValue as RT
+import org.finos.morphir.runtime.RTValue
 import org.finos.morphir.runtime.RTValue.Comparable.orderToInt
 import org.finos.morphir.runtime.RTValue.{
   Primitive,
-  coerceBoolean,
   coerceComparable,
   coerceDecimal,
   coerceFloat,
   coerceInt,
-  coerceList,
   coerceNumeric,
   coerceTuple,
   unwrapNumericWithHelper
 }
 
-import scala.collection.mutable
-
 object AggregateSDK {
 
-  // Aggregations
-
-  val groupBy = DynamicNativeFunction2("groupBy") {
-    (context: NativeContext) => (f: RT.Function, list: RT.List) =>
-      val result = list.value groupBy { a =>
-        context.evaluator.handleApplyResult(Type.UType.Variable("a"), f, a)
-      }
-      val r = result.view.mapValues(l => RT.List(l))
-      RT.Map(mutable.LinkedHashMap.from(r))
+  val concat = DynamicNativeFunction1("concat") {
+    (context: NativeContext) => (list: RTValue.List) =>
+      val flattened = list.elements.flatMap(inner => RTValue.coerceList(inner).elements)
+      RTValue.List(flattened)
   }
 
 }
