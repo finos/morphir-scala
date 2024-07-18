7db818ca4f070d51be8bd8aad259ec87d50bb5d9
Alex Sieland <alexsieland@proton.me>
2024-07-18 11:57:00 -0400

    aggrigate tests

--- examples/morphir-elm-projects/evaluator-tests/src/Morphir/Examples/App/AggregateTests.elm
+++ examples/morphir-elm-projects/evaluator-tests/src/Morphir/Examples/App/AggregateTests.elm
@@ -1,64 +1,219 @@
 module Morphir.Examples.App.AggregateTests exposing (..)
 
 import Dict exposing (Dict)
-import Morphir.Examples.App.TestUtils exposing (..)
 import Morphir.SDK.Aggregate exposing (..)
-import Tuple exposing (..)
-
+import Morphir.Examples.App.TestUtils exposing (..)
 
 {-| Test: Aggregate/groupBy
-expected =
-Dict.fromList
-[ ( "k1\_1"
-, [ ("k1\_1", 1)
-, ("k1\_1", 2)
-, ("k1\_1", 3)
-, ("k1\_1", 4)
-][ ("k1_1", 1)
-, ("k1_1", 2)
-, ("k1_1", 3)
-, ("k1_1", 4)
-]
-, ( "k1\_2",
-, [ ("k1\_2", 5)
-, ("k1\_2", 6)
-, ("k1\_2", 7)
-, ("k1\_2", 8)
-][ ("k1_2", 5)
-, ("k1_2", 6)
-, ("k1_2", 7)
-, ("k1_2", 8)
-]
-][ ( "k1_1"
-, [ ("k1_1", 1)
-, ("k1_1", 2)
-, ("k1_1", 3)
-, ("k1_1", 4)
-]
-, ( "k1_2",
-, [ ("k1_2", 5)
-, ("k1_2", 6)
-, ("k1_2", 7)
-, ("k1_2", 8)
-]
-]
+expected = 
+    Dict.fromList
+    [ ( "k1_1"
+        , [ TestInput1 "k1_1" "k2_1" 1
+        , TestInput1 "k1_1" "k2_1" 2
+        , TestInput1 "k1_1" "k2_2" 3
+        , TestInput1 "k1_1" "k2_2" 4
+        ]
+    , ( "k1_2",
+        , [ TestInput1 "k1_2" "k2_1" 5
+        , TestInput1 "k1_2" "k2_1" 6
+        , TestInput1 "k1_2" "k2_2" 7
+        , TestInput1 "k1_2" "k2_2" 8
+        ]
+    ]
 -}
-aggregateGroupByTest : TestContext -> Dict String (List ( String, Int ))
+aggregateGroupByTest : TestContext -> Dict key (List a)
 aggregateGroupByTest ctx =
     let
         testDataSet =
-            [ ( "k2_1", 1 )
-            , ( "k2_1", 2 )
-            , ( "k2_2", 3 )
-            , ( "k2_2", 4 )
-            , ( "k2_1", 5 )
-            , ( "k2_1", 6 )
-            , ( "k2_2", 7 )
-            , ( "k2_2", 8 )
+            [ TestInput1 "k1_1" "k2_1" 1
+            , TestInput1 "k1_1" "k2_1" 2
+            , TestInput1 "k1_1" "k2_2" 3
+            , TestInput1 "k1_1" "k2_2" 4
+            , TestInput1 "k1_2" "k2_1" 5
+            , TestInput1 "k1_2" "k2_1" 6
+            , TestInput1 "k1_2" "k2_2" 7
+            , TestInput1 "k1_2" "k2_2" 8
             ]
     in
     test ctx <|
         testDataSet
-            |> groupBy
-        <|
-            first
+            |> groupBy .key1
+
+
+{-| Test: Aggregate/aggregate
+expected = 
+    [ { key = "k1_1", count = 4, sum = 10, max = 4, min = 1 }
+    , { key = "k1_2", count = 2, sum = 26, max = 8, min = 5 }
+    ]
+-}
+aggregateAggregateTest : TestContext -> List b
+aggregateAggregateTest ctx =
+    let
+        grouped =
+            Dict.fromList
+                [ ( "k1_1"
+                , [ TestInput1 "k1_1" "k2_1" 1
+                    , TestInput1 "k1_1" "k2_1" 2
+                    , TestInput1 "k1_1" "k2_2" 3
+                    , TestInput1 "k1_1" "k2_2" 4
+                    ]
+                , ( "k1_2",
+                , [ TestInput1 "k1_2" "k2_1" 5
+                    , TestInput1 "k1_2" "k2_1" 6
+                    , TestInput1 "k1_2" "k2_2" 7
+                    , TestInput1 "k1_2" "k2_2" 8
+                    ]
+                ]
+    in
+    grouped
+        |> aggregate
+            (\key inputs ->
+                { key = key
+                , count = inputs (count |> withFilter (\a -> a.value < 7))
+                , sum = inputs (sumOf .value)
+                , max = inputs (maximumOf .value)
+                , min = inputs (minimumOf .value)
+                }
+            )
+
+
+{-| Test: Aggregate/aggregateMap
+expected = 
+    [ ( TestInput1 "k1_1" "k2_1" 1, 10 / 1 )
+    , ( TestInput1 "k1_1" "k2_1" 2, 10 / 2 )
+    , ( TestInput1 "k1_1" "k2_2" 3, 10 / 3 )
+    , ( TestInput1 "k1_1" "k2_2" 4, 10 / 4 )
+    , ( TestInput1 "k1_2" "k2_1" 5, 26 / 5 )
+    , ( TestInput1 "k1_2" "k2_1" 6, 26 / 6 )
+    , ( TestInput1 "k1_2" "k2_2" 7, 26 / 7 )
+    , ( TestInput1 "k1_2" "k2_2" 8, 26 / 8 )
+    ]
+-}
+aggregateAggregateMapTest : TestContext -> List b
+aggregateAggregateMapTest ctx =
+    let
+        testDataSet =
+            [ TestInput1 "k1_1" "k2_1" 1
+            , TestInput1 "k1_1" "k2_1" 2
+            , TestInput1 "k1_1" "k2_2" 3
+            , TestInput1 "k1_1" "k2_2" 4
+            , TestInput1 "k1_2" "k2_1" 5
+            , TestInput1 "k1_2" "k2_1" 6
+            , TestInput1 "k1_2" "k2_2" 7
+            , TestInput1 "k1_2" "k2_2" 8
+            ]
+    in
+    testDataSet
+        |> aggregateMap
+            (sumOf .value |> byKey .key1)
+                (\\totalValue input ->
+                    ( input, totalValue / input.value )
+                )
+
+
+{-| Test: Aggregate/aggregateMap2
+expected = 
+    [ ( TestInput1 "k1_1" "k2_1" 1, 10 * 6 / 1 )
+    , ( TestInput1 "k1_1" "k2_1" 2, 10 * 6 / 2 )
+    , ( TestInput1 "k1_1" "k2_2" 3, 10 * 8 / 3 )
+    , ( TestInput1 "k1_1" "k2_2" 4, 10 * 8 / 4 )
+    , ( TestInput1 "k1_2" "k2_1" 5, 26 * 6 / 5 )
+    , ( TestInput1 "k1_2" "k2_1" 6, 26 * 6 / 6 )
+    , ( TestInput1 "k1_2" "k2_2" 7, 26 * 8 / 7 )
+    , ( TestInput1 "k1_2" "k2_2" 8, 26 * 8 / 8 )
+    ]
+-}
+aggregateAggregateMap2Test : TestContext -> List b
+aggregateAggregateMap2Test ctx =
+    let
+        testDataSet =
+            [ TestInput1 "k1_1" "k2_1" 1
+            , TestInput1 "k1_1" "k2_1" 2
+            , TestInput1 "k1_1" "k2_2" 3
+            , TestInput1 "k1_1" "k2_2" 4
+            , TestInput1 "k1_2" "k2_1" 5
+            , TestInput1 "k1_2" "k2_1" 6
+            , TestInput1 "k1_2" "k2_2" 7
+            , TestInput1 "k1_2" "k2_2" 8
+            ]
+    in
+    testDataSet
+        |> aggregateMap2
+            (sumOf .value |> byKey .key1)
+            (maximumOf .value |> byKey .key2)
+            (\totalValue maxValue input ->
+                ( input, totalValue * maxValue / input.value )
+            )
+
+
+{-| Test: Aggregate/aggregateMap3
+expected = 
+    [ ( TestInput1 "k1_1" "k2_1" 1, 10 * 6 / 1 + 1 )
+    , ( TestInput1 "k1_1" "k2_1" 2, 10 * 6 / 2 + 1 )
+    , ( TestInput1 "k1_1" "k2_2" 3, 10 * 8 / 3 + 3 )
+    , ( TestInput1 "k1_1" "k2_2" 4, 10 * 8 / 4 + 3 )
+    , ( TestInput1 "k1_2" "k2_1" 5, 26 * 6 / 5 + 5 )
+    , ( TestInput1 "k1_2" "k2_1" 6, 26 * 6 / 6 + 5 )
+    , ( TestInput1 "k1_2" "k2_2" 7, 26 * 8 / 7 + 7 )
+    , ( TestInput1 "k1_2" "k2_2" 8, 26 * 8 / 8 + 7 )
+    ]
+-}
+aggregateAggregateMap3Test : TestContext -> List b
+aggregateAggregateMap3Test ctx =
+    let
+        testDataSet =
+            [ TestInput1 "k1_1" "k2_1" 1
+            , TestInput1 "k1_1" "k2_1" 2
+            , TestInput1 "k1_1" "k2_2" 3
+            , TestInput1 "k1_1" "k2_2" 4
+            , TestInput1 "k1_2" "k2_1" 5
+            , TestInput1 "k1_2" "k2_1" 6
+            , TestInput1 "k1_2" "k2_2" 7
+            , TestInput1 "k1_2" "k2_2" 8
+            ]
+    in
+    testDataSet
+        |> aggregateMap3
+            (sumOf .value |> byKey .key1)
+            (maximumOf .value |> byKey .key2)
+            (minimumOf .value |> byKey (key2 .key1 .key2))
+            (\totalValue maxValue minValue input ->
+                ( input, totalValue * maxValue / input.value + minValue )
+            )
+
+
+{-| Test: Aggregate/aggregateMap4
+expected = 
+    [ ( TestInput1 "k1_1" "k2_1" 1, 10 * 6 / 1 + 1 + 1.5 )
+    , ( TestInput1 "k1_1" "k2_1" 2, 10 * 6 / 2 + 1 + 1.5 )
+    , ( TestInput1 "k1_1" "k2_2" 3, 10 * 8 / 3 + 3 + 3.5 )
+    , ( TestInput1 "k1_1" "k2_2" 4, 10 * 8 / 4 + 3 + 3.5 )
+    , ( TestInput1 "k1_2" "k2_1" 5, 26 * 6 / 5 + 5 + 5.5 )
+    , ( TestInput1 "k1_2" "k2_1" 6, 26 * 6 / 6 + 5 + 5.5 )
+    , ( TestInput1 "k1_2" "k2_2" 7, 26 * 8 / 7 + 7 + 7.5 )
+    , ( TestInput1 "k1_2" "k2_2" 8, 26 * 8 / 8 + 7 + 7.5 )
+    ]
+-}
+aggregateAggregateMap4Test : TestContext -> List b
+aggregateAggregateMap4Test ctx =
+    let
+        testDataSet =
+            [ TestInput1 "k1_1" "k2_1" 1
+            , TestInput1 "k1_1" "k2_1" 2
+            , TestInput1 "k1_1" "k2_2" 3
+            , TestInput1 "k1_1" "k2_2" 4
+            , TestInput1 "k1_2" "k2_1" 5
+            , TestInput1 "k1_2" "k2_1" 6
+            , TestInput1 "k1_2" "k2_2" 7
+            , TestInput1 "k1_2" "k2_2" 8
+            ]
+    in
+    testDataSet
+        |> aggregateMap4
+            (sumOf .value |> byKey .key1)
+            (maximumOf .value |> byKey .key2)
+            (minimumOf .value |> byKey (key2 .key1 .key2))
+            (averageOf .value |> byKey (key2 .key1 .key2))
+            (\totalValue maxValue minValue average input ->
+                ( input, totalValue * maxValue / input.value + minValue + average )
+            )
