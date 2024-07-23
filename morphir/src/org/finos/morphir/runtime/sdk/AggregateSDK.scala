b8ec35b2d622035ead676f36b205151ab7ce54a0
Alex Sieland <alexsieland@proton.me>
2024-07-23 17:15:04 -0400

    Aggregate function

--- morphir/src/org/finos/morphir/runtime/sdk/AggregateSDK.scala
+++ morphir/src/org/finos/morphir/runtime/sdk/AggregateSDK.scala
@@ -7,7 +7,7 @@ import org.finos.morphir.runtime.MorphirRuntimeError.{FailedCoercion, IllegalVal
 import org.finos.morphir.runtime.internal.*
 import org.finos.morphir.runtime.RTValue as RT
 import org.finos.morphir.runtime.RTValue.Comparable.orderToInt
-import org.finos.morphir.runtime.RTValue.{Primitive, coerceBoolean, coerceComparable, coerceDecimal, coerceFloat, coerceInt, coerceNumeric, coerceTuple, unwrapNumericWithHelper}
+import org.finos.morphir.runtime.RTValue.{Primitive, coerceBoolean, coerceComparable, coerceDecimal, coerceFloat, coerceInt, coerceList, coerceNumeric, coerceTuple, unwrapNumericWithHelper}
 
 import scala.collection.mutable
 
@@ -22,12 +22,67 @@ object AggregateSDK {
       RT.Map(mutable.LinkedHashMap.from(r))
   }
 
+  val aggregate = DynamicNativeFunction2("aggregate") {
+    (context: NativeContext) => (f: RT.Function, dict: RT.Map) =>
+      val result = dict.value flatMap { case (mapKey, values) =>
+        coerceList(values).value map { a =>
+          context.evaluator.handleApplyResult2(Type.UType.Unit(()), f, mapKey, a)
+        }
+      }
+      RT.List(result.toList)
+  }
+
   val aggregateMap = DynamicNativeFunction3("aggregateMap") {
     (context: NativeContext) => (agg: RT.Aggregation, f: RT.Function, list: RT.List) =>
       val result = list.value map { a =>
         val aggResult = context.evaluator.handleApplyResult(Type.UType.Variable("a"), agg.key, a)
         val input = coerceFloat(aggResult)
         val output = context.evaluator.handleApplyResult2(Type.UType.Variable("a"), f, input, a)
+        RT.Tuple(a, output)
+      }
+      RT.List(result)
+  }
+
+  val aggregate2Map = DynamicNativeFunction4("aggregate2Map") {
+    (context: NativeContext) => (agg1: RT.Aggregation, agg2: RT.Aggregation, f: RT.Function, list: RT.List) =>
+      val result = list.value map { a =>
+        val agg1Result = context.evaluator.handleApplyResult(Type.UType.Variable("a"), agg1.key, a)
+        val agg1Value = coerceFloat(agg1Result)
+        val agg2Result = context.evaluator.handleApplyResult(Type.UType.Variable("a"), agg2.key, a)
+        val agg2Value = coerceFloat(agg2Result)
+        val output = context.evaluator.handleApplyResult3(Type.UType.Variable("a"), f, agg1Value, agg2Value, a)
+        RT.Tuple(a, output)
+      }
+      RT.List(result)
+  }
+
+  val aggregate3Map = DynamicNativeFunction5("aggregate3Map") {
+    (context: NativeContext) => (agg1: RT.Aggregation, agg2: RT.Aggregation, agg3: RT.Aggregation, f: RT.Function, list: RT.List) =>
+      val result = list.value map { a =>
+        val agg1Result = context.evaluator.handleApplyResult(Type.UType.Variable("a"), agg1.key, a)
+        val agg1Value = coerceFloat(agg1Result)
+        val agg2Result = context.evaluator.handleApplyResult(Type.UType.Variable("a"), agg2.key, a)
+        val agg2Value = coerceFloat(agg2Result)
+        val agg3Result = context.evaluator.handleApplyResult(Type.UType.Variable("a"), agg3.key, a)
+        val agg3Value = coerceFloat(agg3Result)
+        val output = context.evaluator.handleApplyResult4(Type.UType.Variable("a"), f, agg1Value, agg2Value, agg3Value, a)
+        RT.Tuple(a, output)
+      }
+      RT.List(result)
+  }
+
+  val aggregate4Map = DynamicNativeFunction6("aggregate4Map") {
+    (context: NativeContext) => (agg1: RT.Aggregation, agg2: RT.Aggregation, agg3: RT.Aggregation, agg4: RT.Aggregation, f: RT.Function, list: RT.List) =>
+      val result = list.value map { a =>
+        val agg1Result = context.evaluator.handleApplyResult(Type.UType.Variable("a"), agg1.key, a)
+        val agg1Value = coerceFloat(agg1Result)
+        val agg2Result = context.evaluator.handleApplyResult(Type.UType.Variable("a"), agg2.key, a)
+        val agg2Value = coerceFloat(agg2Result)
+        val agg3Result = context.evaluator.handleApplyResult(Type.UType.Variable("a"), agg3.key, a)
+        val agg3Value = coerceFloat(agg3Result)
+        val agg4Result = context.evaluator.handleApplyResult(Type.UType.Variable("a"), agg4.key, a)
+        val agg4Value = coerceFloat(agg4Result)
+        val output = context.evaluator.handleApplyResult5(Type.UType.Variable("a"), f, agg1Value, agg2Value, agg3Value, agg4Value, a)
         RT.Tuple(a, output)
       }
       RT.List(result)
