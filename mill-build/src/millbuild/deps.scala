c1a23e74f07d0a9029b74640d934c326beada538
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-06-23 02:37:33 +0200

    Update pprint to 0.9.6 (#837)

--- mill-build/src/millbuild/deps.scala
+++ mill-build/src/millbuild/deps.scala
@@ -62,7 +62,7 @@ object Deps {
       val mainargs       = mvn"com.lihaoyi::mainargs::${Versions.`mainargs`}"
       val `os-lib`       = mvn"com.lihaoyi::os-lib::${Versions.`os-lib`}"
       val sourcecode     = mvn"com.lihaoyi::sourcecode::0.4.2"
-      val pprint         = mvn"com.lihaoyi::pprint::0.9.0"
+      val pprint         = mvn"com.lihaoyi::pprint::0.9.6"
       val ujson          = mvn"com.lihaoyi::ujson::${Versions.upickle}"
       val upickle        = mvn"com.lihaoyi::upickle::${Versions.upickle}"
       val `upickle-core` = mvn"com.lihaoyi::upickle-core::${Versions.upickle}"
