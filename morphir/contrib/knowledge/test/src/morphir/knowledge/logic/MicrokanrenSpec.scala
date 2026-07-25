646418882751c95fb1ae94009c358ff0f2ace1c2
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2026-07-25 15:50:17 -0500

    test: port remaining contrib.knowledge specs to kyo-test
    Mechanical DSL port of FieldSpec/FieldsSpec/StateSpec/MicrokanrenSpec
    from zio-test to kyo-test (suite/test -> -/in, assertTrue(a,b,c) ->
    sequential assert calls). No behavior changes. MicrokanrenSpec
    exercises the migrated Flux engine end-to-end through Goal/Kernel.

--- morphir/contrib/knowledge/test/src/morphir/knowledge/logic/MicrokanrenSpec.scala
+++ morphir/contrib/knowledge/test/src/morphir/knowledge/logic/MicrokanrenSpec.scala
@@ -2,88 +2,84 @@ package morphir.knowledge.logic
 
 import morphir.knowledge.logic.{microkanren => mk}
 import mk._
-import zio.Chunk
-import zio.test._
-import zio.test.Assertion._
-import zio.test.TestAspect.{ignore, tag}
+import kyo.Chunk
+import kyo.test.*
 
-object MicrokanrenSpec extends ZIOSpecDefault {
-  def spec = suite("Fact Spec")(
-    suite("equal")(
-      test("When 2 values are equal we should get back the initial state") {
-        val value1 = BigDecimal(19.99)
-        val value2 = BigDecimal(19.99)
-        val state  = State.empty
-        val sut    = mk.equal(value1, value2)
-        for {
-          actual <- sut(state).runCollect
-        } yield assert(actual)((equalTo(Chunk(state))))
-      },
-      test("Calling equal, eq, and === should be equivalent") {
-        import mk._
-        val value1      = "Test-Value"
-        val value2      = "Test-Value"
-        val state       = State.empty
-        val equalGoal   = mk.equal(value1, value2)
-        val eqGoal      = mk.eq(value1, value2)
-        val equalOpGoal = value1 === value2
-        for {
-          resultsFromEqual   <- equalGoal(state).runCollect
-          resultsFromEq      <- eqGoal(state).runCollect
-          resultsFromEqualOp <- equalOpGoal(state).runCollect
-        } yield assert(resultsFromEq)(equalTo(resultsFromEqual)) &&
-          assert(resultsFromEq)((equalTo(resultsFromEqualOp))) &&
-          assert(resultsFromEqual)((equalTo(resultsFromEqualOp)))
-      }
-    ),
-    suite("or/disjunction")(
-      test("When the first goal succeeds we should get back the state") {
-        import mk._
-        val state       = State.empty
-        val successGoal = Goal.succeed
-        val failingGoal = Goal.fail
-        for {
-          results <- or(successGoal, failingGoal)(state).runCollect
-        } yield assert(results)(equalTo(Chunk(state)))
-      },
-      test("When the first goal fails and second goal succeeds we should get back the state") {
-        import mk._
-        val state       = State.empty
-        val successGoal = Goal.succeed
-        val failingGoal = Goal.fail
-        for {
-          results <- or(failingGoal, successGoal)(state).runCollect
-        } yield assert(results)(equalTo(Chunk(state)))
-      }
-    ),
-    suite("and/conjunction")(
-      test("Both goals must succeed for us to get back the state") {
-        import mk._
-        val state = State.empty
-        val g1    = Goal.succeed
-        val g2    = Goal.succeed
-        for {
-          results <- and(g1, g2)(state).runCollect
-        } yield assert(results)(equalTo(Chunk(state)))
-      },
-      test("If the first goal fails the whole goal fails") {
-        import mk._
-        val state = State.empty
-        val g1    = Goal.fail
-        val g2    = Goal.succeed
-        for {
-          results <- and(g1, g2)(state).runCollect
-        } yield assert(results)(equalTo(Chunk.empty))
-      },
-      test("If the second goal fails the whole goal fails") {
-        import mk._
-        val state = State.empty
-        val g1    = Goal.succeed
-        val g2    = Goal.fail
-        for {
-          results <- and(g1, g2)(state).runCollect
-        } yield assert(results)(equalTo(Chunk.empty))
-      }
-    )
-  )
-}
+class MicrokanrenSpec extends Test[Any]:
+  "equal" - {
+    "When 2 values are equal we should get back the initial state" in {
+      val value1 = BigDecimal(19.99)
+      val value2 = BigDecimal(19.99)
+      val state  = State.empty
+      val sut    = mk.equal(value1, value2)
+      val actual = sut(state).runCollect
+      assert(actual == Chunk(state))
+    }
+
+    "Calling equal, eq, and === should be equivalent" in {
+      import mk._
+      val value1      = "Test-Value"
+      val value2      = "Test-Value"
+      val state       = State.empty
+      val equalGoal   = mk.equal(value1, value2)
+      val eqGoal      = mk.eq(value1, value2)
+      val equalOpGoal = value1 === value2
+
+      val resultsFromEqual   = equalGoal(state).runCollect
+      val resultsFromEq      = eqGoal(state).runCollect
+      val resultsFromEqualOp = equalOpGoal(state).runCollect
+      assert(resultsFromEq == resultsFromEqual)
+      assert(resultsFromEq == resultsFromEqualOp)
+      assert(resultsFromEqual == resultsFromEqualOp)
+    }
+  }
+
+  "or/disjunction" - {
+    "When the first goal succeeds we should get back the state" in {
+      import mk._
+      val state       = State.empty
+      val successGoal = Goal.succeed
+      val failingGoal = Goal.fail
+      val results     = or(successGoal, failingGoal)(state).runCollect
+      assert(results == Chunk(state))
+    }
+
+    "When the first goal fails and second goal succeeds we should get back the state" in {
+      import mk._
+      val state       = State.empty
+      val successGoal = Goal.succeed
+      val failingGoal = Goal.fail
+      val results     = or(failingGoal, successGoal)(state).runCollect
+      assert(results == Chunk(state))
+    }
+  }
+
+  "and/conjunction" - {
+    "Both goals must succeed for us to get back the state" in {
+      import mk._
+      val state   = State.empty
+      val g1      = Goal.succeed
+      val g2      = Goal.succeed
+      val results = and(g1, g2)(state).runCollect
+      assert(results == Chunk(state))
+    }
+
+    "If the first goal fails the whole goal fails" in {
+      import mk._
+      val state   = State.empty
+      val g1      = Goal.fail
+      val g2      = Goal.succeed
+      val results = and(g1, g2)(state).runCollect
+      assert(results == Chunk.empty)
+    }
+
+    "If the second goal fails the whole goal fails" in {
+      import mk._
+      val state   = State.empty
+      val g1      = Goal.succeed
+      val g2      = Goal.fail
+      val results = and(g1, g2)(state).runCollect
+      assert(results == Chunk.empty)
+    }
+  }
+end MicrokanrenSpec
--- morphir/contrib/knowledge/test/src/morphir/knowledge/logic/core/FieldSpec.scala
+++ morphir/contrib/knowledge/test/src/morphir/knowledge/logic/core/FieldSpec.scala
@@ -1,15 +1,14 @@
 package morphir.knowledge.logic.core
-import zio.test._
 
-object FieldSpec extends ZIOSpecDefault {
-  def spec = suite("FieldSpec")(
-    suite("define") {
-      test("define is able to get the name from the variable it is being defined on") {
-        val snoop   = Field.define[String]
-        val marshal = Field.define[Int]
-        val dre     = Field.define[Double]
-        assertTrue(snoop.name == "snoop", marshal.name == "marshal", dre.name == "dre")
-      }
-    }
-  )
-}
+import kyo.test.*
+
+class FieldSpec extends Test[Any]:
+  "define is able to get the name from the variable it is being defined on" in {
+    val snoop   = Field.define[String]
+    val marshal = Field.define[Int]
+    val dre     = Field.define[Double]
+    assert(snoop.name == "snoop")
+    assert(marshal.name == "marshal")
+    assert(dre.name == "dre")
+  }
+end FieldSpec
