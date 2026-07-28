324936d94a98592ca36d09a273d837778a3ed346
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-07-28 23:47:19 +0200

    Update izumi-reflect to 3.0.9 (#838)

--- mill-build/src/millbuild/deps.scala
+++ mill-build/src/millbuild/deps.scala
@@ -204,7 +204,7 @@ object Versions {
   val fansi                      = "0.5.1"
   val fs2                        = "3.13.0"
   val geny                       = "1.1.1"
-  val `izumi-reflect`            = "2.3.10"
+  val `izumi-reflect`            = "3.0.9"
   val `kyo-case-app`             = "1.0.0-RC5"
   val metaconfig                 = "0.12.0"
   val mainargs                   = "0.7.8"
