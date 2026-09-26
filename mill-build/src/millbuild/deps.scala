94c74524e3e57ba3aef2db6a70b4b39fb2d9ba35
Scala Steward <scala_steward@virtuslab.com>
2026-09-26 16:45:40 +0000

    Update zio-process to 0.8.1

--- mill-build/src/millbuild/deps.scala
+++ mill-build/src/millbuild/deps.scala
@@ -298,7 +298,7 @@ object Versions {
   val `zio-nio`          = "2.0.2"
   val `zio-parser`       = "0.1.11"
   val `zio-prelude`      = "1.0.0-RC48"
-  val `zio-process`      = "0.8.0"
+  val `zio-process`      = "0.8.1"
   val `zio-schema`       = "0.4.12"
 }
 
